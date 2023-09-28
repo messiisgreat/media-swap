c1a7df6ca6b7dfe4722067778e3181e05bfa7148
messiisgreat <superonecoderman@gmail.com>
2023-09-28 22:49:40 +0900

    プロダクト詳細ページにタグを表示

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -18,6 +18,20 @@ const getProduct = cache(async (id: string) => {
   return product;
 });
 
+const getTags = cache(async (ids: string[]) => {
+  if (!ids.length) return [];
+
+  const tags = await prisma.tag.findMany({
+    where: {
+      id: {
+        in: ids
+      }
+    }
+  });
+
+  return tags;
+});
+
 export async function generateMetadata({
   params: { id },
 }: ProductPageProps): Promise<Metadata> {
@@ -36,6 +50,7 @@ export default async function ProductPage({
   params: { id },
 }: ProductPageProps) {
   const product = await getProduct(id);
+  const tags = await getTags(product.tagIds);
 
   return (
     <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
@@ -50,6 +65,13 @@ export default async function ProductPage({
 
       <div>
         <h1 className="text-5xl font-bold">{product.name}</h1>
+        <div className="mt-4 flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <span key={tag.id} className="bg-yellow-400 px-3 py-1 rounded-full font-medium shadow-md">
+              {tag.name}
+            </span>
+          ))}
+        </div>
         <PriceTag price={product.price} className="mt-4" />
         <p className="py-6">{product.description}</p>
         <BuyItemButton productId={product.id} />
