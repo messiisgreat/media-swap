879e7c2971a385fa30b40d5b41b812d19375c6e2
messiisgreat <superonecoderman@gmail.com>
2023-10-07 19:58:13 +0900

    電磁交付規約ページ

--- src/app/digital/page.tsx
+++ src/app/digital/page.tsx
@@ -1,3 +1,9 @@
 export default function DigitalPage() {
-  return <div>page</div>;
+  return (
+    <div>
+      <div className="border-2">
+        <h2 className="text-center text-xl font-bold">電磁交付規約</h2>
+      </div>
+    </div>
+  );
 }
