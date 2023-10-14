1bd4fc5e86f9ec2ffe0af95d16e010a646daee94
messiisgreat <superonecoderman@gmail.com>
2023-10-14 21:38:19 +0900

    年齢認証リダイレクトのバグを修正

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,19 +3,20 @@ import type { NextRequest } from "next/server";
 
 export async function middleware(req: NextRequest) {
     const url = req.url;
-    console.log('リダイレクトURL' + url);
+    console.log('リダイレクトURL: ' + url);
 
-    const ageUrls = <string[]>[
-        'http://localhost:3000/age-check',
-        'https://www.swappy.jp/age-check',
-    ];
+    const parsedUrl = new URL(url);
+    const pathName = parsedUrl.pathname;
+    console.log(pathName);
 
     const isAgeCheckedThrough = req.cookies.get('isAgeCheckedThrough')?.value;
+    const isAgeCheckPath = pathName == '/age-check';
+    console.log(isAgeCheckPath);
 
-    if (isAgeCheckedThrough == undefined) {
+    if (!isAgeCheckPath && isAgeCheckedThrough == undefined) {
         console.log('年齢認証画面にリダイレクト');
         return NextResponse.redirect(new URL('/age-check', req.url))
-    } else if (ageUrls.includes(url)) {
+    } else if (isAgeCheckPath && isAgeCheckedThrough != undefined) {
         // age-check が終わってるけどリダイレクトしてしまった場合はホームに返す
         return NextResponse.redirect(new URL('/', req.url))
     }
