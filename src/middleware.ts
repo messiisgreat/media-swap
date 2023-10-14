aa6af8f2881ba85cd13b769c89174abcb50e722b
messiisgreat <superonecoderman@gmail.com>
2023-10-14 16:32:32 +0900

    年齢制限へのリダイレクト処理を追加

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,30 @@
 import { NextResponse } from "next/server";
-import { getServerSession } from "next-auth";
 import type { NextRequest } from "next/server";
-import { authOptions } from "./app/api/auth/[...nextauth]/route";
 
 export async function middleware(req: NextRequest) {
-    return NextResponse.redirect(new URL('/age-check', req.url))
+    const url = req.url;
+    console.log('リダイレクトURL' + url);
+
+    const ageUrls = <string[]>[
+        'http://localhost:3000/age-check',
+        'https://www.swappy.jp/age-check',
+    ];
+
+    const isAgeCheckedThrough = req.cookies.get('isAgeCheckedThrough')?.value;
+
+    if (isAgeCheckedThrough == undefined) {
+        console.log('年齢認証画面にリダイレクト');
+        return NextResponse.redirect(new URL('/age-check', req.url))
+    } else if (ageUrls.includes(url)) {
+        // age-check が終わってるけどリダイレクトしてしまった場合はホームに返す
+        return NextResponse.redirect(new URL('/', req.url))
+    }
 }
 
 export const config = {
     matcher: [
         '/',
+        '/age-check',
         '/products/:path*',
     ],
 }
