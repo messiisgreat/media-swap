46da7749f2d387cc5fc159eff53f8904cb43f327
messiisgreat <superonecoderman@gmail.com>
2023-10-21 19:06:32 +0900

    いいえ後の×マークの差替

--- src/app/no-available-service/page.tsx
+++ src/app/no-available-service/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import closeIcon from "/src/assets/close.jpeg";
+import closeIcon from "/src/assets/close.webp";
 
 export default function NoAvailableServicePage() {
   return (
