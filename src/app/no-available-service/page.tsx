89e95294a346d17f7aee25625d2b4c2ecbfeff81
messiisgreat <superonecoderman@gmail.com>
2023-10-21 19:36:19 +0900

    文字の改行

--- src/app/no-available-service/page.tsx
+++ src/app/no-available-service/page.tsx
@@ -7,7 +7,10 @@ export default function NoAvailableServicePage() {
       <Image src={closeIcon} alt="" width={120} height={120} />
       <h1 className="my-10 text-4xl font-bold text-red-500">年齢認証</h1>
       <p className="font-bold text-orange-600">
-        このサイトはアダルトコンテンツを含むため、18歳未満の方はご利用いただけません
+        このサイトはアダルトコンテンツを含む為
+      </p>
+      <p className="font-bold text-orange-600">
+        18歳未満の方はご利用いただけません
       </p>
     </div>
   );
