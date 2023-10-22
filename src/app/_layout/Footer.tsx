b02063fb6c7083b1307a8fd9ad2c74ca6ee813d4
messiisgreat <superonecoderman@gmail.com>
2023-10-22 18:12:07 +0900

    footerのメニュー開閉追加

--- src/app/_layout/Footer.tsx
+++ src/app/_layout/Footer.tsx
@@ -1,16 +1,7 @@
-import Link from "next/link";
-import { FaFacebookSquare } from "react-icons/fa";
-import { FaSquareXTwitter } from "react-icons/fa6";
-
-type LinkObject = {
-  text: string;
-  url: string;
-};
-
-type LinkSection = {
-  title: string;
-  items: LinkObject[];
-};
+"use client";
+import { useEffect, useState } from "react";
+import { FooterIcons } from "@/app/_layout/FooterIcons";
+import { FooterContent, FooterMobileContent } from "@/app/_layout/FooterContents";
 
 /**
  * サイトのフッター
@@ -18,39 +9,14 @@ type LinkSection = {
  * @returns footer
  */
 export function Footer() {
-  const links: LinkSection[] = [
-    {
-      title: "Swappyについて",
-      items: [
-        { text: "会社概要(運営会社)", url: "#" },
-        { text: "採用情報", url: "#" },
-      ],
-    },
-    {
-      title: "ヘルプ",
-      items: [{ text: "お問い合わせ", url: "#" }],
-    },
-    {
-      title: "プライバシーと利用規約",
-      items: [
-        { text: "プライバシーポリシー", url: "/static/privacy-policy/" },
-        { text: "外部送信ポリシー", url: "/static/cookie-policy/" },
-        { text: "Swappy利用規約", url: "/static/tos/" },
-        { text: "電磁交付規約", url: "/static/digital/" },
-        { text: "コンプライアンスポリシー", url: "/static/compliance/" },
-        {
-          text: "個人データの安全管理に係る基本方針",
-          url: "/static/data-security/",
-        },
-        { text: "特定商取引に関する表記", url: "/static/tokutei/" },
-        { text: "資金決済法に基づく表示", url: "/static/shikin-kessai/" },
-        { text: "法務確認について", url: "/static/legal-check/" },
-      ],
-    },
-  ];
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    setIsMobile(window.matchMedia("(max-width: 768px)").matches);
+  }, [isMobile]);
 
   return (
     <footer className="bg-neutral p-10 text-neutral-content">
+<<<<<<< HEAD
       <div className="footer m-auto max-w-7xl">
         {/* // TODO: 利用規約ページを作成する */}
         <div>
@@ -109,6 +75,14 @@ export function Footer() {
           </div>
         </div>
       </div>
+=======
+      {isMobile ? (
+        <FooterMobileContent />
+      ) : (
+        <FooterContent />
+      )}
+      <FooterIcons />
+>>>>>>> 2beeabc (footerのメニュー開閉追加)
     </footer>
   );
 }
