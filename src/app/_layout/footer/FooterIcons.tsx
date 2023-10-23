8999bfcf245df5b5f57727a2a51dd441e9abeb08
messiisgreat <superonecoderman@gmail.com>
2023-10-23 18:18:09 +0900

    todoコメントの戻

--- src/app/_layout/footer/FooterIcons.tsx
+++ src/app/_layout/footer/FooterIcons.tsx
@@ -6,9 +6,11 @@ export const FooterIcons = () => {
     <>
       <div className="m-auto max-w-7xl md:flex md:flex-grow md:justify-between">
         <div className="my-2 flex gap-2">
+          {/* todo: twitterのリンクを追加する */}
           <Link href="https://twitter.com/home" target="_blank">
             <FaSquareXTwitter size={30} />
           </Link>
+          {/* todo: facebookのリンクを追加する */}
           <Link href="https://www.facebook.com/" target="_blank">
             <FaFacebookSquare size={30} />
           </Link>
