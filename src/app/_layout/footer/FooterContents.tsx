26e810c3903bba76f24ef99fefa8dc51a8b6f66c
messiisgreat <superonecoderman@gmail.com>
2023-10-23 21:53:42 +0900

    関数コメントの追記及びkeyの再設定

--- src/app/_layout/footer/FooterContents.tsx
+++ src/app/_layout/footer/FooterContents.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link";
 import { links } from "@/app/_layout/LinkContents";
 
+/**
+ * モバイル用のフッター
+ * @returns footer
+ */
 export const FooterMobileContent = () => {
   return (
     <ul className="menu px-0">
       <li>
-        {links.map((section, index) => (
-          <details key={index}>
+        {links.map((section) => (
+          <details key={section.title}>
             <summary className="footer-title px-0 hover:text-white">
               {section.title}
             </summary>
             <ul className="mx-0 px-0">
-              {section.items.map((item, i) => (
-                <li key={i} color="">
+              {section.items.map((item) => (
+                <li key={item.text} color="">
                   <Link
                     href={item.url}
                     className="px-0 font-medium hover:text-white hover:underline "
@@ -29,16 +33,20 @@ export const FooterMobileContent = () => {
   );
 };
 
+/**
+ * PC用のフッター
+ * @returns footer
+ */
 export const FooterContent = () => {
   return (
     <div className="footer m-auto max-w-7xl">
-      {links.map((section, index) => (
-        <div key={index}>
+      {links.map((section) => (
+        <div key={section.title}>
           <span className="footer-title">{section.title}</span>
-          {section.items.map((item, i) => (
+          {section.items.map((item) => (
             <Link
               href={item.url}
-              key={i}
+              key={item.text}
               className="font-medium hover:underline"
             >
               {item.text}
