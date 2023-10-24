f4baa678438c5b98b42f9930d7dba17173aa48c9
messiisgreat <superonecoderman@gmail.com>
2023-10-24 20:12:47 +0900

    初期化クエリ変更

--- etc/init/init.js
+++
@@ -1,8 +0,0 @@
-rs.initiate({
-  _id: "replset",
-  members: [
-    { _id: 0, host: "mongo-primary:27017", priority: 100 },
-    { _id: 1, host: "mongo-secondary:27017", priority: 10 },
-    { _id: 2, host: "mongo-arbiter:27017", arbiterOnly: true },
-  ],
-});
