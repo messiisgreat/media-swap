6ca03aeef86e42f2257c8f58739516f41f446640
messiisgreat <superonecoderman@gmail.com>
2023-10-24 12:30:36 +0900

    fix(Products): Heroに移行

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -54,37 +54,39 @@ export default async function ProductPage({
   const tags = await getTags(product.tagIds);
 
   return (
-    <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
-      <Image
-        src={product.imageUrl}
-        alt={product.name}
-        width={500}
-        height={500}
-        className="rounded-lg"
-        priority
-      />
+    <div className="hero">
+      <div className="hero-content flex-col lg:flex-row lg:items-center lg:-ml-[12%]">
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={500}
+          height={500}
+          className="rounded-lg"
+          priority
+        />
 
-      <div>
-        <h1 className="text-5xl font-bold">{product.name}</h1>
-        {/* <div className="mt-4 flex flex-wrap gap-2">
+        <div>
+          <h1 className="text-5xl font-bold">{product.name}</h1>
+          {/* <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <span key={tag.id} className="bg-yellow-400 px-3 py-1 rounded-full font-medium shadow-md">
               {tag.name}
             </span>
           ))}
         </div> */}
-        <div className="mt-4 flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <Link key={tag.id} href={`/search?tagid=${tag.id}`}>
-              <Badge className="cursor-pointer bg-yellow-400 badge-lg border-none font-medium shadow-md">
-                {tag.text}
-              </Badge>
-            </Link>
-          ))}
+          <div className="mt-4 flex flex-wrap gap-2">
+            {tags.map((tag) => (
+              <Link key={tag.id} href={`/search?tagid=${tag.id}`}>
+                <Badge className="badge-lg cursor-pointer border-none bg-yellow-400 font-medium shadow-md">
+                  {tag.text}
+                </Badge>
+              </Link>
+            ))}
+          </div>
+          <Badge className="mt-4">¥{product.price}</Badge>
+          <p className="py-6">{product.description}</p>
+          <BuyItemButton productId={product.id} />
         </div>
-        <Badge className="mt-4">¥{product.price}</Badge>
-        <p className="py-6">{product.description}</p>
-        <BuyItemButton productId={product.id} />
       </div>
     </div>
   );
