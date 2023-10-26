479fdfcb5cfea594fe4b75892e8df43fb9cd00d8
messiisgreat <superonecoderman@gmail.com>
2023-10-26 22:12:22 +0900

    終了ステータスをチェックするようにしました

--- docker_init.sh
+++ docker_init.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
 # MongoDB接続情報
-MONGO_HOST="localhost" # あなたのホストのMongoDBのアドレス
-MONGO_PORT="27018" # docker-composeで指定したポート
+MONGO_HOST=${MONGO_HOST:-"localhost"}
+MONGO_PORT=${MONGO_PORT:-"27018"}
 
 # レプリカセットの初期化をリトライ
 for i in {1..5}; do
     OUTPUT=$(mongosh --quiet --host $MONGO_HOST --port $MONGO_PORT --eval 'rs.initiate({ _id: "replset", members: [ { _id: 0, host: "mongo-primary:27017", priority: 100 }, { _id: 1, host: "mongo-secondary:27017", priority: 10 }, { _id: 2, host: "mongo-arbiter:27017", arbiterOnly: true } ] });' 2>&1)
-    SUCCESS=$(echo $OUTPUT | grep 'ok: 1')
+    STATUS=$?
 
-    if [ -n "$SUCCESS" ]; then
+   if [ $STATUS -eq 0 ]; then
         echo "Replica set initiated successfully on attempt $i."
         break
     else
