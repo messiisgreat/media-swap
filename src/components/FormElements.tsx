1e1db48651662f984cd84946985973936bc8b50d
messiisgreat <superonecoderman@gmail.com>
2023-10-30 02:08:01 +0900

    refactor:ImageInputを修正

--- src/components/FormElements.tsx
+++ src/components/FormElements.tsx
@@ -23,21 +23,24 @@ export const Textarea = forwardRef<
   return <textarea className={textareaClass} {...props} ref={ref} />;
 });
 
+/**
+ * 画像を選択するinputタグにCSSを適用したラッパー
+ * @param id 一意のIDを指定する clientではuseID, serverではcuidを使用する
+ * @param props inputタグのattribute
+ * @returns label
+ */
 export const ImageInput = forwardRef<
   HTMLInputElement,
   ComponentPropsWithoutRef<"input">
 >(function ImageInput({ className, id, ...props }, ref) {
   const labelClass = `flex cursor-pointer items-center justify-center 
-  rounded-md border border-red-500 bg-white text-red-500 
-  hover:border-rose-400 hover:bg-red-50 hover:text-rose-400 ${className ?? ""}`;
+  gap-1 rounded-md border border-red-500 bg-white 
+  py-3.5 text-red-500
+  hover:bg-red-50 ${className ?? ""}`;
   return (
-    <>
-      <label className={labelClass} htmlFor={id}>
-        <div className="flex flex-row items-center justify-center gap-1 px-3 py-3.5">
-          <BiSolidCamera size={20} />
-          <p className="font-bold">画像を選択する</p>
-        </div>
-      </label>
+    <label className={labelClass} htmlFor={id}>
+      <BiSolidCamera size={20} />
+      <p className="font-bold">画像を選択する</p>
       <input
         required
         type="file"
@@ -47,6 +50,6 @@ export const ImageInput = forwardRef<
         ref={ref}
         {...props}
       />
-    </>
+    </label>
   );
 });
