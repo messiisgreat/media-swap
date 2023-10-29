031a0fa003aea6e79107e6166cf504b92b100845
messiisgreat <superonecoderman@gmail.com>
2023-10-30 01:49:02 +0900

    fix:動作修正

--- src/components/securityVerifier/useSecurityVerifier.tsx
+++ src/components/securityVerifier/useSecurityVerifier.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useRef } from "react";
+import { useRef, useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import { z } from "zod";
 
@@ -10,15 +10,18 @@ import { z } from "zod";
  */
 export const useSecurityVerifier = () => {
   const captchaRef = useRef<ReCAPTCHA>(null);
-  const verifiedValue = captchaRef.current?.getValue();
-  const SecurityVerifier = () => (
+  const [verifiedValue, setVerifiedValue] = useState<string | null>(null);
+
+  const SecurityVerifier = (
     <ReCAPTCHA
       sitekey={z
         .string()
         .nonempty()
-        .parse(process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY)}
+        .parse(process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY || "")}
       ref={captchaRef}
+      onChange={(value) => setVerifiedValue(value)}
     />
   );
+
   return [verifiedValue, SecurityVerifier] as const;
 };
