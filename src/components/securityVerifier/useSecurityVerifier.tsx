76ddd2f174c1bb768f2b8a7c1597e1f57fb8229d
messiisgreat <superonecoderman@gmail.com>
2023-10-30 20:41:50 +0900

    refactor:不要なref削除

--- src/components/securityVerifier/useSecurityVerifier.tsx
+++ src/components/securityVerifier/useSecurityVerifier.tsx
@@ -1,15 +1,15 @@
 "use client";
 
-import { useRef, useState } from "react";
+import { useState } from "react";
 import ReCAPTCHA from "react-google-recaptcha";
 import { z } from "zod";
 
 /**
  * セキュリティ認証情報を取得するRenderHook
+ * reCAPTCHAを実装している
  * @returns [verifiedValue, SecurityVerifier] 認証結果, 認証用コンポーネント
  */
 export const useSecurityVerifier = () => {
-  const captchaRef = useRef<ReCAPTCHA>(null);
   const [verifiedValue, setVerifiedValue] = useState<string | null>(null);
 
   const SecurityVerifier = (
@@ -18,7 +18,6 @@ export const useSecurityVerifier = () => {
         .string()
         .nonempty()
         .parse(process.env.NEXT_PUBLIC_GOOGLE_RECAPTCHA_SITE_KEY || "")}
-      ref={captchaRef}
       onChange={(value) => setVerifiedValue(value)}
     />
   );
