950a56a0e08147cc02fd6f5c9754c20f8bf9d11e
messiisgreat <superonecoderman@gmail.com>
2023-11-02 03:10:26 +0900

    fix:staticをルーティング対象外に

---
+++ src/app/_layout/footer/LinkContents.ts
@@ -0,0 +1,42 @@
+import { SITE_NAME } from "const";
+
+export type LinkObject = {
+  text: string;
+  url: string;
+};
+
+export type LinkSection = {
+  title: string;
+  items: LinkObject[];
+};
+
+export const links: LinkSection[] = [
+  {
+    title: `${SITE_NAME}について`,
+    items: [
+      { text: "会社概要(運営会社)", url: "#" },
+      { text: "採用情報", url: "#" },
+    ],
+  },
+  {
+    title: "ヘルプ",
+    items: [{ text: "お問い合わせ", url: "#" }],
+  },
+  {
+    title: "プライバシーと利用規約",
+    items: [
+      { text: "プライバシーポリシー", url: "/privacy-policy/" },
+      { text: "外部送信ポリシー", url: "/cookie-policy/" },
+      { text: `${SITE_NAME}利用規約`, url: "/tos/" },
+      { text: "電磁交付規約", url: "/digital/" },
+      { text: "コンプライアンスポリシー", url: "/compliance/" },
+      {
+        text: "個人データの安全管理に係る基本方針",
+        url: "/data-security/",
+      },
+      { text: "特定商取引に関する表記", url: "/tokutei/" },
+      { text: "資金決済法に基づく表示", url: "/shikin-kessai/" },
+      { text: "法務確認について", url: "/legal-check/" },
+    ],
+  },
+];
