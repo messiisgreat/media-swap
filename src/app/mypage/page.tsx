863cc5b22958f39d1d744552f169b84fdac65bbd
messiisgreat <superonecoderman@gmail.com>
2023-11-01 19:51:37 +0900

    :recycle:aタグの位置修正。レビュー対応

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -16,14 +16,14 @@ export default async function Page() {
     <div className="w-full">
       <div className="m-auto px-4 sm:px-8">
         <ul className="overflow-hidden rounded border border-gray-200 shadow-md">
-          <a href="/mypage/personal-info">
-            <li className="border-b border-gray-200 bg-white px-4 py-2 transition-all duration-300 ease-in-out last:border-none hover:bg-sky-100 hover:text-sky-900">
+          <li className="border-b border-gray-200 bg-white px-4 py-2 transition-all duration-300 ease-in-out last:border-none hover:bg-sky-100 hover:text-sky-900">
+            <a href="/mypage/personal-info">
               <div className="flex justify-between">
                 <p>個人情報設定</p>
                 <FiChevronRight size={24} />
               </div>
-            </li>
-          </a>
+            </a>
+          </li>
         </ul>
       </div>
     </div>
