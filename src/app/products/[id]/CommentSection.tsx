fb89d4e3b595c0d3b464734678ea0f8cf6738a84
messiisgreat <superonecoderman@gmail.com>
2023-11-01 07:26:12 +0100

    feat(Products): [WIP] コメントの追加

---
+++ src/app/products/[id]/CommentSection.tsx
@@ -0,0 +1,110 @@
+"use client";
+
+import { parseRelativeTime } from "@/utils/parseRelativeTime";
+import { Session } from "next-auth";
+import Image from "next/image";
+import { useEffect, useState } from "react";
+
+// TODO: DBにコメントの型定義を作成。今は仮の型定義
+type Comment = {
+  id: string;
+  user: {
+    name: string;
+    iconUrl: string;
+  };
+  comment: string;
+  createdAt: Date;
+};
+
+function Skeleton() {
+  return (
+    <div role="status" className="flex w-full animate-pulse items-center gap-2">
+      <div className="h-16 w-16 flex-none items-center justify-center rounded-full bg-gray-400"></div>
+      <div className="w-full">
+        <div className="mb-2.5 h-2 max-w-[360px] rounded-full bg-gray-400"></div>
+        <div className="mb-2.5 h-2 rounded-full bg-gray-400"></div>
+        <div className="mb-2.5 h-2 max-w-[330px] rounded-full bg-gray-400"></div>
+        <div className="h-2 max-w-[360px] rounded-full bg-gray-400"></div>
+      </div>
+      <span className="sr-only">Loading...</span>
+    </div>
+  );
+}
+
+/**
+ * コメント(+コメントを書き込むフォーム)
+ * @param param0.productId 商品ID
+ */
+export default function CommentSection({
+  productId,
+  session,
+}: {
+  productId: string;
+  session: Session | null;
+}) {
+  const [comments, setComments] = useState<Comment[] | null>(null);
+
+  useEffect(() => {
+    setComments([
+      {
+        id: "1",
+        user: {
+          name: "ユーザー1",
+          iconUrl: "https://picsum.photos/200",
+        },
+        comment: "コメント1",
+        createdAt: new Date(),
+      },
+    ]);
+
+    // TODO: DBからコメントを取得する処理を実装
+    //fetchComments(productId).then((comments) => setComments(comments));
+  }, []);
+
+  return (
+    <div className="mx-auto w-full">
+      <p className="mb-2 text-xl font-medium">コメント</p>
+      {!session ? (
+        <form className="flex flex-col items-start gap-4">
+          <textarea className="textarea textarea-bordered w-full resize-none"></textarea>
+          <button className="btn btn-secondary">コメントを書き込む</button>
+        </form>
+      ) : (
+        <p>コメントを書き込むにはログインが必要です。</p>
+      )}
+      <ul className="mt-8 flex w-full">
+        {!comments ? (
+          <div className="flex w-full flex-col gap-6">
+            <Skeleton />
+            <Skeleton />
+          </div>
+        ) : comments.length === 0 ? (
+          <p className="text-center">コメントはありません。</p>
+        ) : (
+          comments.map((comment) => (
+            <li key={comment.id} className="flex flex-1 items-center gap-4">
+              <div className="h-16 w-16 flex-none items-center justify-center rounded-full bg-gray-400">
+                <Image
+                  src={comment.user.iconUrl}
+                  alt={comment.user.name}
+                  className="rounded-full"
+                  width={64}
+                  height={64}
+                />
+              </div>
+              <div className="flex w-full flex-col">
+                <div className="flex items-center justify-between">
+                  <p className="text-lg font-bold">{comment.user.name}</p>{" "}
+                  <p className="text-sm text-gray-400">
+                    {parseRelativeTime(comment.createdAt)}
+                  </p>
+                </div>
+                <p className="text-sm">{comment.comment}</p>
+              </div>
+            </li>
+          ))
+        )}
+      </ul>
+    </div>
+  );
+}
--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -5,6 +5,8 @@ import { Metadata } from "next";
 import Image from "next/image";
 import Link from "next/link";
 import BuyItemButton from "./BuyItemButton";
+import CommentSection from "@/app/products/[id]/CommentSection";
+import { getServerSession } from "next-auth";
 
 type ProductPageProps = {
   params: {
@@ -38,42 +40,47 @@ export default async function ProductPage({
 }: ProductPageProps) {
   const product = await findProduct(id);
   const tags = await findTagsByIds(product.tagIds);
+  const session = await getServerSession();
 
   return (
-    <div className="hero">
-      <div className="hero-content flex-col lg:-ml-[12%] lg:flex-row lg:items-center">
-        <Image
-          src={product.imageUrl}
-          alt={product.name}
-          width={500}
-          height={500}
-          className="rounded-lg"
-          priority
-        />
+    <div>
+      <div className="hero">
+        <div className="hero-content flex-col gap-8 lg:flex-row lg:items-center">
+          <Image
+            src={product.imageUrl}
+            alt={product.name}
+//            width={500}
+//            height={500}
+            className="!static rounded-lg object-contain lg:max-w-lg flex-1"
+            fill
+            priority
+          />
 
-        <div>
-          <h1 className="text-5xl font-bold">{product.name}</h1>
-          {/* <div className="mt-4 flex flex-wrap gap-2">
+          <div className="flex-1">
+            <h1 className="text-5xl font-bold">{product.name}</h1>
+            {/* <div className="mt-4 flex flex-wrap gap-2">
           {tags.map((tag) => (
             <span key={tag.id} className="bg-yellow-400 px-3 py-1 rounded-full font-medium shadow-md">
               {tag.name}
             </span>
           ))}
         </div> */}
-          <div className="mt-4 flex flex-wrap gap-2">
-            {tags.map((tag) => (
-              <Link key={tag.id} href={`/search?tagid=${tag.id}`}>
-                <Badge className="badge-lg cursor-pointer border-none bg-yellow-400 font-medium shadow-md">
-                  {tag.text}
-                </Badge>
-              </Link>
-            ))}
+            <div className="mt-4 flex flex-wrap gap-2">
+              {tags.map((tag) => (
+                <Link key={tag.id} href={`/search?tagid=${tag.id}`}>
+                  <Badge className="badge-lg cursor-pointer border-none bg-yellow-400 font-medium shadow-md">
+                    {tag.text}
+                  </Badge>
+                </Link>
+              ))}
+            </div>
+            <Badge className="mt-4">¥{product.price}</Badge>
+            <p className="py-6">{product.description}</p>
+            <BuyItemButton productId={product.id} />
           </div>
-          <Badge className="mt-4">¥{product.price}</Badge>
-          <p className="py-6">{product.description}</p>
-          <BuyItemButton productId={product.id} />
         </div>
       </div>
+      <CommentSection productId={product.id} session={session} />
     </div>
   );
 }
