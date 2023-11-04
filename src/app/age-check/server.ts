72b812e64d3f779c7df4fff67721cc5ecdf715c9
messiisgreat <superonecoderman@gmail.com>
2023-11-04 21:53:56 +0900

    不要なasync削除

--- src/app/age-check/server.ts
+++ src/app/age-check/server.ts
@@ -4,9 +4,7 @@ import { cookies } from "next/headers";
 
 /**
  * 年齢確認済みフラグをCookieに追加する
- * @returns {Promise<void>}
- * @todo Promiseを返していない気がするのでasync不要では？
  */
-export const addAgeCheckedCookie = async (): Promise<void> => {
+export const addAgeCheckedCookie = () => {
   cookies().set("isAgeCheckedThrough", "true");
 };
