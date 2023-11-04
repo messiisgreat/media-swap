31256c4bc11fedc5963883993e423191e792e015
messiisgreat <superonecoderman@gmail.com>
2023-11-04 12:58:09 +0900

    型定義

---
+++ src/app/add-listing/types.ts
@@ -0,0 +1,10 @@
+export type ProductFormData = {
+  productName: string;
+  price: number;
+  description: string;
+  shippingDays: string;
+  shippingMethod: string;
+  productCondition: string;
+  imageFile: File;
+  tags: string;
+};
