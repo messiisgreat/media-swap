335f4092f1cc54ac42bfe2222f8c9169ff12d431
messiisgreat <superonecoderman@gmail.com>
2023-11-09 00:31:09 +0900

    IDを24文字に修正

--- src/constants/listing.ts
+++ src/constants/listing.ts
@@ -8,11 +8,11 @@ import {
 
 /** 商品の状態 */
 export const PRODUCT_CONDITION: ProductCondition[] = [
-  { id: "0", name: "新品または未使用" },
-  { id: "1", name: "目立った傷や汚れなし" },
-  { id: "2", name: "やや傷や汚れあり" },
-  { id: "3", name: "傷や汚れあり" },
-  { id: "4", name: "全体的に状態が悪い" },
+  { id: "0".padStart(24, "0"), name: "新品または未使用" },
+  { id: "1".padStart(24, "0"), name: "目立った傷や汚れなし" },
+  { id: "2".padStart(24, "0"), name: "やや傷や汚れあり" },
+  { id: "3".padStart(24, "0"), name: "傷や汚れあり" },
+  { id: "4".padStart(24, "0"), name: "全体的に状態が悪い" },
 ];
 
 /** 配送料の負担 */
@@ -22,35 +22,35 @@ export const POSTAGE_IS_INCLUDED = [
 ];
 /** 配送方法 */
 export const SHIPPING_METHOD: ShippingMethod[] = [
-  { id: "0", name: "未定", amount: null },
-  { id: "1", name: "らくらくメルカリ便", amount: 500 },
-  { id: "2", name: "ゆうメール", amount: 200 },
-  { id: "3", name: "レターパック", amount: 500 },
-  { id: "4", name: "普通郵便(定形、定形外)", amount: null },
-  { id: "5", name: "クロネコヤマト", amount: 800 },
-  { id: "6", name: "ゆうパック", amount: 800 },
-  { id: "7", name: "クリックポスト", amount: 200 },
-  { id: "8", name: "ゆうパケット", amount: 200 },
+  { id: "0".padStart(24, "0"), name: "未定", amount: null },
+  { id: "1".padStart(24, "0"), name: "らくらくメルカリ便", amount: 500 },
+  { id: "2".padStart(24, "0"), name: "ゆうメール", amount: 200 },
+  { id: "3".padStart(24, "0"), name: "レターパック", amount: 500 },
+  { id: "4".padStart(24, "0"), name: "普通郵便(定形、定形外)", amount: null },
+  { id: "5".padStart(24, "0"), name: "クロネコヤマト", amount: 800 },
+  { id: "6".padStart(24, "0"), name: "ゆうパック", amount: 800 },
+  { id: "7".padStart(24, "0"), name: "クリックポスト", amount: 200 },
+  { id: "8".padStart(24, "0"), name: "ゆうパケット", amount: 200 },
 ];
 /** 配送日の目安 */
 export const SHIPPING_DAYS: ShippingDays[] = [
-  { id: "0", name: "1~2日で発送", maxDays: 2 },
-  { id: "1", name: "2~3日で発送", maxDays: 3 },
-  { id: "2", name: "4~7日で発送", maxDays: 7 },
+  { id: "0".padStart(24, "0"), name: "1~2日で発送", maxDays: 2 },
+  { id: "1".padStart(24, "0"), name: "2~3日で発送", maxDays: 3 },
+  { id: "2".padStart(24, "0"), name: "4~7日で発送", maxDays: 7 },
 ];
 
 /** 取引ステータス */
 export const TRANSACTION_STATUS: TransactionStatus[] = [
-  { id: "0", name: "支払い前", step: 0 },
-  { id: "1", name: "支払い完了", step: 1 },
-  { id: "2", name: "発送済み", step: 2 },
-  { id: "3", name: "受け取り完了", step: 3 },
-  { id: "4", name: "取引キャンセル", step: 4 },
+  { id: "0".padStart(24, "0"), name: "支払い前", step: 0 },
+  { id: "1".padStart(24, "0"), name: "支払い完了", step: 1 },
+  { id: "2".padStart(24, "0"), name: "発送済み", step: 2 },
+  { id: "3".padStart(24, "0"), name: "受け取り完了", step: 3 },
+  { id: "4".padStart(24, "0"), name: "取引キャンセル", step: 4 },
 ];
 
 /** 取引評価選択肢 */
 export const TRANSACTION_RATING_OPTION: TransactionRatingOption[] = [
-  { id: "1", name: "良い", rating: 1 },
-  { id: "2", name: "普通", rating: 0 },
-  { id: "3", name: "悪い", rating: -1 },
+  { id: "1".padStart(24, "0"), name: "良い", rating: 1 },
+  { id: "2".padStart(24, "0"), name: "普通", rating: 0 },
+  { id: "3".padStart(24, "0"), name: "悪い", rating: -1 },
 ];
