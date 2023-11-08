8498460ae880b7455ca25e1086628ef4e14b6dc0
messiisgreat <superonecoderman@gmail.com>
2023-11-08 19:30:09 +0900

    単体のファイルをuploadする関数に分離

--- src/lib/ImageUploadCloudinary.ts
+++ src/lib/ImageUploadCloudinary.ts
@@ -1,9 +1,21 @@
 import { v2 as cloudinary } from "cloudinary";
 
 const cloudinaryConfig = {
-  cloud_name: process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME ?? (() => { throw new Error("Cloudinary cloud name is not set"); })(),
-  api_key: process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY ?? (() => { throw new Error("Cloudinary API key is not set"); })(),
-  api_secret: process.env.CLOUDINARY_API_SECRET ?? (() => { throw new Error("Cloudinary API secret is not set"); })(),
+  cloud_name:
+    process.env.NEXT_PUBLIC_CLOUDINARY_CLOUDNAME ??
+    (() => {
+      throw new Error("Cloudinary cloud name is not set");
+    })(),
+  api_key:
+    process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY ??
+    (() => {
+      throw new Error("Cloudinary API key is not set");
+    })(),
+  api_secret:
+    process.env.CLOUDINARY_API_SECRET ??
+    (() => {
+      throw new Error("Cloudinary API secret is not set");
+    })(),
   secure: true,
 };
 
@@ -21,6 +33,41 @@ async function getSignature(): Promise<{
   return { timestamp, signature };
 }
 
+async function uploadSingleFile(
+  file: File,
+  timestamp: number,
+  signature: string,
+  apiKey: string,
+  uploadUrl: string
+): Promise<string> {
+  const formData = new FormData();
+  formData.append("file", file);
+  formData.append("api_key", apiKey);
+  formData.append("signature", signature);
+  formData.append("timestamp", timestamp.toString());
+  formData.append("folder", "swappy");
+
+  try {
+    const response = await fetch(uploadUrl, {
+      method: "POST",
+      body: formData,
+    });
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const data = await response.json();
+    if (data.secure_url) {
+      return data.secure_url;
+    } else {
+      throw new Error("Cloudinary upload failed");
+    }
+  } catch (error: any) {
+    throw new Error(`Failed to upload file: ${error.message}`);
+  }
+}
+
 /**
  * 画像をCloudinaryにアップロードする
  * @param files ユーザーがドロップしたファイル
@@ -32,27 +79,13 @@ export async function uploadToCloudinary(files: File[]): Promise<string[]> {
   const uploadPromises: Promise<string>[] = [];
 
   files.forEach((file) => {
-    const formData = new FormData();
-    formData.append("file", file);
-    formData.append("api_key", process.env.NEXT_PUBLIC_CLOUDINARY_API_KEY!);
-    formData.append("signature", signature);
-    formData.append("timestamp", timestamp.toString());
-    formData.append("folder", "swappy");
-
-    const endpoint = process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_URL!;
-
-    const uploadPromise = fetch(endpoint, {
-      method: "POST",
-      body: formData,
-    })
-      .then((res) => res.json())
-      .then((data) => {
-        if (data.secure_url) {
-          return data.secure_url;
-        } else {
-          throw new Error("Cloudinary upload failed");
-        }
-      });
+    const uploadPromise = uploadSingleFile(
+      file,
+      timestamp,
+      signature,
+      cloudinaryConfig.api_key!,
+      process.env.NEXT_PUBLIC_CLOUDINARY_UPLOAD_URL!,
+    );
 
     uploadPromises.push(uploadPromise);
   });
