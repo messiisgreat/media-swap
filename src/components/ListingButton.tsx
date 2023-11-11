8aab3b1109f4248cee75fc16eef385c856435dd0
messiisgreat <superonecoderman@gmail.com>
2023-11-12 01:28:50 +0900

    出品ボタン関数を分離

---
+++ src/components/ListingButton.tsx
@@ -0,0 +1,9 @@
+import { ButtonAsLink } from "@/components/Button";
+
+/**
+ * 商品出品ボタン
+ * @returns
+ */
+export const ListingButton = () => {
+  return <ButtonAsLink href="/add-listing">出品する</ButtonAsLink>;
+};
