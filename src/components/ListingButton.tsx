43e74adb118aa4abef7142d7f90fba64b697241d
messiisgreat <superonecoderman@gmail.com>
2023-11-13 01:18:31 +0900

    仮の出品ボタン作成

--- src/components/ListingButton.tsx
+++ src/components/ListingButton.tsx
@@ -1,9 +1,22 @@
-import { ButtonAsLink } from "@/components/Button";
+import Link from "next/link";
+import { LuBox } from "react-icons/lu";
 
 /**
  * 商品出品ボタン
  * @returns
  */
-export const ListingButton = () => {
-  return <ButtonAsLink href="/add-listing">出品する</ButtonAsLink>;
+export const ListingButton = ({ className }: { className?: string }) => {
+  return (
+    <Link
+      href="/add-listing"
+      title="出品する"
+      className={`flex h-16 w-16 flex-col items-center justify-center gap-1 
+      rounded-full bg-primary font-bold text-white hover:bg-primary-focus ${
+        className ? className : ""
+      }`}
+    >
+      <LuBox size={28} />
+      <span className="text-xs">出品</span>
+    </Link>
+  );
 };
