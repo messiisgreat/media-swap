854e5ad8828a4c03c80074eafbe5c4368519f265
messiisgreat <superonecoderman@gmail.com>
2023-11-13 17:21:22 +0900

    authMiddleware作成

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,19 @@
+import { getToken } from "next-auth/jwt";
+import { ComposableMiddleware } from "next-compose-middleware";
+import { NextResponse } from "next/server";
+
+/**
+ * 認証していない場合にログイン画面にリダイレクトするミドルウェア
+ * @param req NextRequest
+ * @param res NextResponse
+ */
+export const authMiddleware: ComposableMiddleware = async (req, res) => {
+  const token = await getToken({ req, secret: process.env.SECRET });
+  if (!token) {
+    const pathName = new URL(req.url).pathname;
+    return NextResponse.redirect(
+      new URL(`/api/auth/signin?callbackUrl=/${encodeURI(pathName)}`, req.url),
+    );
+  }
+  return res;
+};
