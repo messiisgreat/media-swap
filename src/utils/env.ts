b0da4ea878596c1f6cabeb490d0f1f45a70a8225
messiisgreat <superonecoderman@gmail.com>
2023-11-16 01:34:30 +0900

    省略

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,16 +1,16 @@
-import zod from "zod";
+import z from "zod";
 
-const envSchema = zod.object({
-  DATABASE_URL: zod.string().min(1),
-  GOOGLE_CLIENT_ID: zod.string().min(1),
-  GOOGLE_CLIENT_SECRET: zod.string().min(1),
-  NEXTAUTH_URL: zod.string().min(1),
-  NEXTAUTH_SECRET: zod.string().min(1),
-  GOOGLE_RECAPTCHA_SECRET_KEY: zod.string().min(1),
-  CLOUDINARY_CLOUDNAME: zod.string().min(1),
-  CLOUDINARY_API_KEY: zod.string().min(1),
-  CLOUDINARY_API_SECRET: zod.string().min(1),
-  CLOUDINARY_UPLOAD_URL: zod.string().min(1),
+const envSchema = z.object({
+  DATABASE_URL: z.string().min(1),
+  GOOGLE_CLIENT_ID: z.string().min(1),
+  GOOGLE_CLIENT_SECRET: z.string().min(1),
+  NEXTAUTH_URL: z.string().min(1),
+  NEXTAUTH_SECRET: z.string().min(1),
+  GOOGLE_RECAPTCHA_SECRET_KEY: z.string().min(1),
+  CLOUDINARY_CLOUDNAME: z.string().min(1),
+  CLOUDINARY_API_KEY: z.string().min(1),
+  CLOUDINARY_API_SECRET: z.string().min(1),
+  CLOUDINARY_UPLOAD_URL: z.string().min(1),
 });
 
 export const env = envSchema.parse(process.env);
