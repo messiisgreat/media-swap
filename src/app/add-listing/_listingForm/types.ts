e8d4b3d95e08442e25db30df65bd6a19ff175f62
messiisgreat <superonecoderman@gmail.com>
2023-11-17 00:08:35 +0900

    zod schemaのバリデーションをいろいろ追加

--- src/app/add-listing/_listingForm/types.ts
+++ src/app/add-listing/_listingForm/types.ts
@@ -36,21 +36,33 @@ export const initialProductFormValues: ProductFormState = {
 };
 
 export const ProductFormSchema: ZodType<ProductFormData> = z.object({
-  productName: z.string().min(1, { message: "商品名は必須です" }),
+  productName: z
+    .string()
+    .min(1, { message: "商品名は必須です" })
+    .max(32, { message: "商品名は32文字以内で入力してください" }),
   productConditionId: z
     .string()
     .min(1, { message: "商品の状態を選択してください" }),
-  price: z.number().min(0, { message: "価格は0以上で入力してください" }),
-  description: z.string().min(1, { message: "商品の説明は必須です" }),
+  price: z
+    .number()
+    .min(1, { message: "価格は1円以上で入力してください" })
+    .max(9999999, { message: "価格は9999999円以下で入力してください" }),
+  description: z
+    .string()
+    .min(1, { message: "商品の説明は必須です" })
+    .max(1000, { message: "商品の説明は1000文字以内で入力してください" }),
   postageIsIncluded: z.string().min(1, { message: "送料を選択してください" }),
   shippingDaysId: z.string().min(1, { message: "発送日数を選択してください" }),
   shippingMethodId: z
     .string()
     .min(1, { message: "発送方法を選択してください" }),
-  imageFiles: z.array(z.any()).min(1, { message: "画像は必須です" }),
+  imageFiles: z
+    .array(z.custom<File>())
+    .nonempty({ message: "画像は必須です" })
+    .max(10, { message: "画像は10枚まで選択できます" }),
   tags: z.string(),
   verificationCode: z
     .string({ required_error: "認証を行ってください" })
     .min(1, { message: "認証を行ってください" }),
-  isPublic: z.string().min(1, { message: "公開設定は必須です" }),
+  isPublic: z.string().min(1),
 });
