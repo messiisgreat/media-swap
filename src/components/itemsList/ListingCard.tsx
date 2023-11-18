11e4ecf42eca220083bf10f444b67500c16c6ecf
messiisgreat <superonecoderman@gmail.com>
2023-11-18 09:45:40 +0900

    top pageのリサイズ

--- src/components/itemsList/ListingCard.tsx
+++ src/components/itemsList/ListingCard.tsx
@@ -3,6 +3,15 @@ import { findListingById } from "@/services/listing";
 import { formatPrice } from "@/utils/format";
 import Image from "next/image";
 import Link from "next/link";
+import { env } from "@/utils/env";
+
+function transformCloudinaryURL(url: string, width: number, height: number, backgroundColor: string): string {
+  const baseUrl = `https://res.cloudinary.com/${env.CLOUDINARY_CLOUDNAME}/image/upload/`;
+  const transformation = `c_pad,b_rgb:${backgroundColor},w_${width},h_${height}`;
+  const imagePath = url.split(baseUrl)[1];
+
+  return `${baseUrl}${transformation}/${imagePath}`;
+}
 
 type Props = {
   listing: Awaited<ReturnType<typeof findListingById>>;
@@ -25,7 +34,7 @@ export function ListingCard({ listing }: Props) {
       >
         <div className="relative h-32 w-32 cursor-pointer rounded-lg sm:h-48 sm:w-48">
           <Image
-            src={listing.images[0].imageURL}
+            src={transformCloudinaryURL(listing.images[0].imageURL, 500, 500, '031e2b')}
             alt={listing.productName || "Product Image"}
             layout="fill"
             objectFit="cover"
