b596c02b6a3475dba8ebb1bde746ddbc9e752ad8
messiisgreat <superonecoderman@gmail.com>
2023-11-19 06:25:35 +0900

    画像サイズの変更

--- src/components/itemsList/ListingCard.tsx
+++ src/components/itemsList/ListingCard.tsx
@@ -23,12 +23,12 @@ export function ListingCard({ listing }: Props) {
         href={`/listing/${listing.id}`}
         className="card w-full bg-base-100 transition-shadow hover:shadow-xl"
       >
-        <div className="relative h-32 w-32 cursor-pointer rounded-lg sm:h-48 sm:w-48">
+        <div className="relative w-[calc((100vw-32px-4px*2)/3)] h-[calc((100vw-32px+4px*2)/3)] cursor-pointer rounded-lg sm:h-48 sm:w-48">
           <Image
             src={listing.images[0].imageURL}
             alt={listing.productName || "Product Image"}
             fill
-            sizes=""
+            sizes="100vw"
             className="rounded-lg"
           />
           <PriceBadge className="absolute bottom-2  inline-flex h-6 w-16 items-baseline overflow-hidden whitespace-nowrap rounded-r-lg bg-black/40 pb-2 text-xs">
