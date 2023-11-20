5b6302058299c3552e6babe9f2454656313c1b6a
messiisgreat <superonecoderman@gmail.com>
2023-11-20 22:57:54 +0900

    textarea以外設定可能に

--- src/app/add-listing/_listingForm/TestDataButton.tsx
+++ src/app/add-listing/_listingForm/TestDataButton.tsx
@@ -1,24 +1,32 @@
 import { Button } from "@/components";
+import {
+  PRODUCT_CONDITION,
+  SHIPPING_DAYS,
+  SHIPPING_METHOD,
+} from "@/constants/listing";
 import { ComponentProps } from "react";
 
-function setTestData(selector: string, testData: string): void {
+const setTestData = (selector: string, testData: string): void => {
   const element = document.querySelector(selector);
 
   if (element) {
-    element.value = testData;
+    if (element instanceof HTMLTextAreaElement) {
+      element.innerHTML = testData;
+    } else if (
+      element instanceof HTMLInputElement ||
+      element instanceof HTMLSelectElement
+    ) {
+      element.value = testData;
+    } else {
+      console.error(`Unsupported element type for selector ${selector}.`);
+    }
   } else {
     console.error(`Element with selector ${selector} not found.`);
   }
-}
+};
 
-const handleClick = () => {
-  setTestData("#productName", "試験商品");
-  setTestData("#productConditionId", "1");
-  setTestData("#price", "999");
-  setTestData("#description", "これは試験商品の説明です。");
-  setTestData("#postageIsIncluded", "1");
-  setTestData("#shippingDaysId", "1");
-  setTestData("#shippingMethodId", "1");
+const generateRandomId = (constant: { id: string; name: string }[]): string => {
+  return constant[Math.floor(Math.random() * constant.length)].id.toString();
 };
 
 type Props = ComponentProps<typeof Button>;
@@ -28,6 +36,24 @@ type Props = ComponentProps<typeof Button>;
  * @param props ボタンのprops
  */
 export const TestDataButton = (props: Props) => {
+  const handleClick = () => {
+    setTestData('[name="productName"]', "試験商品");
+    setTestData(
+      '[name="productConditionId"]',
+      generateRandomId(PRODUCT_CONDITION),
+    );
+    setTestData(
+      '[name="price"]',
+      Math.floor(Math.random() * 99999700 + 300).toString(),
+    );
+    setTestData('[name="description"]', "これは試験商品の説明です。");
+    setTestData(
+      '[name="postageIsIncluded"]',
+      Math.floor(Math.random() * 2).toString(),
+    );
+    setTestData('[name="shippingDaysId"]', generateRandomId(SHIPPING_DAYS));
+    setTestData('[name="shippingMethodId"]', generateRandomId(SHIPPING_METHOD));
+  };
   return (
     <Button onClick={handleClick} {...props}>
       試験データ設定
