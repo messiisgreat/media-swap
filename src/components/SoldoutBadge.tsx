1eb3fe3ba62f81f3eacb908161ea06e5402a321d
messiisgreat <superonecoderman@gmail.com>
2023-11-21 23:45:43 +0900

    classNameをTailwind cssのdefaultで書く

--- src/components/SoldoutBadge.tsx
+++ src/components/SoldoutBadge.tsx
@@ -3,7 +3,7 @@
  */
 const SoldOutBadge = () => {
   return (
-    <div className="flex h-[50px] w-[50px] justify-center bg-[red] [clipPath:polygon(0_0,_100%_0,0_100%)]">
+    <div className="flex h-12 w-12 justify-center bg-red-600 [clipPath:polygon(0_0,_100%_0,0_100%)]">
       <span className="-rotate-45  p-2 text-xs font-bold text-white">SOLD</span>
     </div>
   );
