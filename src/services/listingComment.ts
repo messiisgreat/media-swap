6948ff59e996522803a81f2f5b1b21a1799e9e4b
messiisgreat <superonecoderman@gmail.com>
2023-11-21 04:48:38 +0100

    feat: コメントの削除機能

--- src/services/listingComment.ts
+++ src/services/listingComment.ts
@@ -13,12 +13,14 @@ export type CommentWithPartialUser = ListingComment & {
  */
 export const getComments = async (
   listingId: string,
-): Promise<CommentWithPartialUser[]> =>
-  prisma.listingComment.findMany({
+): Promise<CommentWithPartialUser[]> => {
+  const comments = await prisma.listingComment.findMany({
     where: { listingId },
     orderBy: [{ createdAt: "desc" }, { id: "desc" }],
     include: { user: { select: { name: true, image: true } } },
   });
+  return comments.filter((comment) => !comment.deletedAt);
+}
 
 /**
  * コメントを追加する
@@ -45,7 +47,7 @@ export async function createComment(
  * @param commentId コメントのID
  * @param userId 通報ユーザーID
  * @param reason 通報理由
- * @returns 
+ * @returns
  * @throws 通報済みの場合
  */
 export async function createCommentReport(
@@ -53,11 +55,55 @@ export async function createCommentReport(
   userId: string,
   reason: string,
 ) {
+  // 既に同じユーザーによる通報があるか確認
+  const existingReport = await prisma.listingCommentReport.findFirst({
+    where: {
+      listingCommentId: commentId,
+      userId: userId,
+    },
+  });
+
+  if (existingReport) {
+    throw new Error("This comment has already been reported by the user.");
+  }
+
   return prisma.listingCommentReport.create({
     data: {
       listingCommentId: commentId,
       userId,
       reason,
+    },
+  });
+}
+
+/**
+ * コメントを削除する
+ * @param commentId コメントID
+ * @param userId 削除を行うユーザーID
+ * @returns 
+ * @throws コメントが見つからない場合
+ * @throws 出品者以外が削除しようとした場合
+ */
+export async function deleteListingComment(
+  commentId: string,
+  userId: string,
+) {
+  const comment = await prisma.listingComment.findUnique({
+    where: { id: commentId },
+    include: { listing: { select: { sellerId: true } } }
+  });
+  if (!comment) {
+    throw new Error("Comment not found");
+  }
+
+  if (comment.listing.sellerId !== userId) {
+    throw new Error("You are not the seller of this listing");
+  }
+
+  return prisma.listingComment.update({
+    where: { id: commentId },
+    data: {
+      deletedAt: new Date(),
     },
   });
 }
