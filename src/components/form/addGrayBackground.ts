60cd11766d42b1c42790d975ba1c007f4fd94a0d
messiisgreat <superonecoderman@gmail.com>
2023-11-23 08:08:10 +0900

    readerの削除

--- src/components/form/addGrayBackground.ts
+++ src/components/form/addGrayBackground.ts
@@ -4,52 +4,43 @@
  */
 export async function addGrayBackground(file: File): Promise<File> {
   return new Promise((resolve, reject) => {
-    const reader = new FileReader();
-    reader.onload = (event: ProgressEvent<FileReader>) => {
-      if (!event.target || !event.target.result) {
-        reject(new Error ("FileReader did not load the file."));
+    const img = new window.Image();
+    const objectUrl = URL.createObjectURL(file);
+    img.src = objectUrl;
+    img.onload = () => {
+      // オブジェクトURLの解放
+      URL.revokeObjectURL(objectUrl);
+      const maxLength = Math.max(img.width, img.height);
+      const canvas = document.createElement('canvas');
+      canvas.width = maxLength;
+      canvas.height = maxLength;
+      const ctx = canvas.getContext('2d');
+
+      if (!ctx) {
+        reject(new Error("Could not create canvas context."));
         return;
       }
 
-      const img = new window.Image();
-      const objectUrl = URL.createObjectURL(file);
-      img.src = objectUrl;
-      img.onload = () => {
-        // オブジェクトURLの解放
-        URL.revokeObjectURL(objectUrl);
-        const maxLength = Math.max(img.width, img.height);
-        const canvas = document.createElement('canvas');
-        canvas.width = maxLength;
-        canvas.height = maxLength;
-        const ctx = canvas.getContext('2d');
+      // 灰色の背景を設定
+      ctx.fillStyle = '#808080';
+      ctx.fillRect(0, 0, maxLength, maxLength);
 
-        if (!ctx) {
-          reject(new Error("Could not create canvas context."));
+      // 画像を中央に配置
+      const offsetX = (maxLength - img.width) / 2;
+      const offsetY = (maxLength - img.height) / 2;
+      ctx.drawImage(img, offsetX, offsetY, img.width, img.height);
+
+      canvas.toBlob((blob) => {
+        if (!blob) {
+          reject(new Error("Canvas toBlob failed."));
           return;
         }
-
-        // 灰色の背景を設定
-        ctx.fillStyle = '#808080';
-        ctx.fillRect(0, 0, maxLength, maxLength);
-
-        // 画像を中央に配置
-        const offsetX = (maxLength - img.width) / 2;
-        const offsetY = (maxLength - img.height) / 2;
-        ctx.drawImage(img, offsetX, offsetY, img.width, img.height);
-
-        canvas.toBlob((blob) => {
-          if (!blob) {
-            reject(new Error("Canvas toBlob failed."));
-            return;
-          }
-          resolve(new File([blob], file.name, { type: 'image/jpeg', lastModified: Date.now() }));
-        }, 'image/jpeg', 1);
-      };
-      img.onerror = () => {
-        URL.revokeObjectURL(objectUrl);
-        reject(new Error("Image loading error."))
-      };
+        resolve(new File([blob], file.name, { type: 'image/jpeg', lastModified: Date.now() }));
+      }, 'image/jpeg', 1);
     };
-    reader.onerror = () => reject(new Error("FileReader error."));
+    img.onerror = () => {
+      URL.revokeObjectURL(objectUrl);
+      reject(new Error("Image loading error."))
+    };
   });
 }
