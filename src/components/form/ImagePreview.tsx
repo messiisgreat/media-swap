588f71327abddf7319136ff29aa70b418a1ab3e1
messiisgreat <superonecoderman@gmail.com>
2023-11-23 04:27:09 +0900

    出品画像プレビューに拡大設定

---
+++ src/components/form/ImagePreview.tsx
@@ -0,0 +1,25 @@
+import { useImageModal } from "@/components/dialog";
+import Image from "next/image";
+
+type Props = {
+  file: File & { preview: string };
+};
+
+/**
+ * 画像をプレビュー表示する
+ */
+export const ImagePreview = ({ file }: Props) => {
+  const { open, ImageModal } = useImageModal(file.preview);
+  return (
+    <>
+      <Image
+        src={file.preview}
+        alt={file.name}
+        width={80}
+        height={80}
+        onClick={open}
+      />
+      <ImageModal />
+    </>
+  );
+};
