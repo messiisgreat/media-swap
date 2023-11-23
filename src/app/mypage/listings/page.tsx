6dfc435bddfe7efede4bc245b3f988948016bae6
messiisgreat <superonecoderman@gmail.com>
2023-11-23 22:03:13 +0900

    fix: isMyListingをpropsで渡すように変更

--- src/app/mypage/listings/page.tsx
+++ src/app/mypage/listings/page.tsx
@@ -8,6 +8,7 @@ type Props = {
     size: number;
     sort: keyof Listing;
     order: "asc" | "desc";
+    isMyListings: boolean;
   };
 };
 
@@ -15,11 +16,23 @@ type Props = {
  *  出品商品一覧を表示するページ
  */
 export default function page({
-  searchParams: { page = 1, size = 27, sort = "createdAt", order = "desc" },
+  searchParams: {
+    page = 1,
+    size = 27,
+    sort = "createdAt",
+    order = "desc",
+    isMyListings = true,
+  },
 }: Props) {
   return (
     <>
-      <ItemsListContainer page={page} size={size} sort={sort} order={order} isMyListings={true} />
+      <ItemsListContainer
+        page={page}
+        size={size}
+        sort={sort}
+        order={order}
+        isMyListings={isMyListings}
+      />
     </>
   );
 }
