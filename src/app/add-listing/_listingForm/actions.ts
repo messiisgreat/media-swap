818fa69361460ff30f7d9b3c11f20c368edb5f18
messiisgreat <superonecoderman@gmail.com>
2023-11-25 02:29:05 +0900

    確認コードのフォームにおいての検証ロジックを切り出し

--- src/app/add-listing/_listingForm/actions.ts
+++ src/app/add-listing/_listingForm/actions.ts
@@ -6,7 +6,7 @@ import {
   ProductFormState,
 } from "@/app/add-listing/_listingForm/types";
 import { getFormValues } from "@/components/form/utils";
-import { fetchVerifyResult } from "@/components/securityVerifier/fetcher";
+import { verifyForm } from "@/components/securityVerifier/verifyForm";
 import { uploadToCloudinary } from "@/lib/ImageUploadCloudinary";
 import {
   UnregisteredListing,
@@ -60,32 +60,28 @@ export const listingItem = async (
       message: "セッションが切れました。再度ログインしてください。",
     };
   }
-  if (!values.isPublic) {
-    const listing = await create(rest, userId, previousPrice);
-    redirect(`/add-listing/complete?listing_id=${listing.id}&is_public=false`);
+
+  const verifyResult = await verifyForm(prevState, verificationCode);
+
+  if (verifyResult) {
+    return verifyResult;
   } else {
-    const validated = ProductFormSchema.safeParse(values);
-    if (!validated.success) {
-      return {
-        ...prevState,
-        errors: validated.error.flatten().fieldErrors,
-        message: "入力に不備があります",
-      };
+    if (!values.isPublic) {
+      const listing = await create(rest, userId, previousPrice);
+      redirect(
+        `/add-listing/complete?listing_id=${listing.id}&is_public=false`,
+      );
+    } else {
+      const validated = ProductFormSchema.safeParse(values);
+      if (!validated.success) {
+        return {
+          ...prevState,
+          errors: validated.error.flatten().fieldErrors,
+          message: "入力に不備があります",
+        };
+      }
+      const listing = await create(rest, userId, previousPrice);
+      redirect(`/add-listing/complete?listing_id=${listing.id}&is_public=true`);
     }
-    if (!verificationCode) {
-      return {
-        ...prevState,
-        message: "認証を行ってください",
-      };
-    }
-    const verifyResult = await fetchVerifyResult(verificationCode);
-    if (!verifyResult) {
-      return {
-        ...prevState,
-        message: "認証に失敗しました。再度認証を行ってください",
-      };
-    }
-    const listing = await create(rest, userId, previousPrice);
-    redirect(`/add-listing/complete?listing_id=${listing.id}&is_public=true`);
   }
 };
---
+++ src/components/securityVerifier/verifyForm.ts
@@ -0,0 +1,29 @@
+import { FormState } from "@/components/form";
+import { fetchVerifyResult } from "@/components/securityVerifier/fetcher";
+
+/**
+ * 認証コードを検証する
+ * @param prevState 前の状態
+ * @param verificationCode 認証コード
+ * @returns エラーメッセージを含んだ状態を返す。認証に成功した場合はnullを返す
+ */
+export const verifyForm = async <T>(
+  prevState: FormState<T>,
+  verificationCode: string,
+): Promise<FormState<T> | null> => {
+  if (!verificationCode) {
+    return {
+      ...prevState,
+      message: "認証コードが送信されていません",
+    };
+  }
+  const verifyResult = await fetchVerifyResult(verificationCode);
+  if (!verifyResult) {
+    return {
+      ...prevState,
+      message: "認証に失敗しました。時間を置いてお試しください。",
+    };
+  } else {
+    return null;
+  }
+};
