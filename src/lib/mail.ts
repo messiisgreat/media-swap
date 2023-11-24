559f7b091f29917e92975bcbcd7eac39793f8995
messiisgreat <superonecoderman@gmail.com>
2023-11-25 01:17:20 +0900

    add:メール送信用関数作成

---
+++ src/lib/mail.ts
@@ -0,0 +1,64 @@
+import { SITE_NAME } from "@/constants/site";
+import { env } from "@/utils/serverEnv";
+import { createTransport, SendMailOptions } from "nodemailer";
+
+const sendMail = async (options: SendMailOptions): Promise<boolean> => {
+  const transporter = createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.GMAIL_ADDRESS,
+      pass: process.env.GMAIL_PASSWORD,
+    },
+  });
+
+  return transporter
+    .sendMail(options)
+    .then(() => {
+      return true;
+    })
+    .catch((error) => {
+      console.log(error);
+      return false;
+    });
+};
+
+/**
+ * 管理者にメールを送信する
+ * 問い合わせフォームなどで使用する
+ * @param from 送信者名
+ * @param subject 件名
+ * @param text 本文
+ */
+export const sendMailToAdmin = async (
+  from: string,
+  subject: string,
+  text: string,
+) => {
+  const options = {
+    from,
+    to: env.GMAIL_ADDRESS,
+    subject,
+    text,
+  };
+  return sendMail(options);
+};
+
+/**
+ * ユーザーにメールを送信する
+ * @param to 送信先メールアドレス
+ * @param subject 件名
+ * @param text 本文
+ */
+export const sendMailToUser = async (
+  to: string,
+  subject: string,
+  text: string,
+) => {
+  const options = {
+    from: `${SITE_NAME} 運営事務局 <${env.GMAIL_ADDRESS}>`,
+    to,
+    subject,
+    text,
+  };
+  return sendMail(options);
+};
