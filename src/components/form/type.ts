a2d6900fa2be09289a88d74af29a980e3d2097ce
messiisgreat <superonecoderman@gmail.com>
2023-11-25 02:41:38 +0900

    型の微修正

--- src/components/form/type.ts
+++ src/components/form/type.ts
@@ -1,7 +1,7 @@
 /** フォームに定義されるべき状態の共通型 */
 export type FormState<T> = {
-  /** 型引数そのもの */
-  values: T;
+  /** 型引数と確認コード */
+  values: T & { verificationCode: string };
   /** 型引数のkeyに対するエラーメッセージを保持する */
   errors?: Partial<Record<keyof T, string[]>>;
   /** そのフォーム全体に対するメッセージ */
