6435d5ecfa64f3f9ef5e5773fe0210b3a50337a8
messiisgreat <superonecoderman@gmail.com>
2023-11-27 18:18:39 +0900

    feat: heicを使ってみたけどエラー出る

--- src/lib/ImageUploadCloudinary.ts
+++ src/lib/ImageUploadCloudinary.ts
@@ -1,4 +1,28 @@
 import { env } from "@/utils/serverEnv";
+import heic2any from "heic2any";
+
+/**
+ * HEIF/HEIC形式の画像をJPEGに変換する関数
+ * @param imageFile 変換前の画像
+ * @returns 変換後の画像
+ */
+// HEIF/HEIC形式の画像をJPEGに変換する関数
+export const convertImage = async (imageFile: File): Promise<Blob> => {
+  if (imageFile.type === "image/heif" || imageFile.type === "image/heic") {
+    const output = await heic2any({
+      blob: imageFile,
+      toType: "image/jpeg",
+    });
+
+    // output が Blob[] の場合は最初の Blob を使用
+    if (Array.isArray(output)) {
+      return output[0];
+    }
+
+    return output;
+  }
+  return imageFile;
+};
 
 async function uploadSingleFile(
   file: File,
@@ -6,7 +30,11 @@ async function uploadSingleFile(
   upload_preset: string,
 ): Promise<string> {
   const formData = new FormData();
-  formData.append("file", file);
+
+  // 画像の変換処理(HEIF/HEIC -> JPEG)
+  const convertedFile = await convertImage(file);
+
+  formData.append("file", convertedFile);
   formData.append("folder", "swappy");
   formData.append("upload_preset", upload_preset);
   formData.append("fetch_format", "auto");
