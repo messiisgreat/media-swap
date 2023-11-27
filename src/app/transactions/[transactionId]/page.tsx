cdb45ce87486e0db2010af813875e196ad3e1c8f
messiisgreat <superonecoderman@gmail.com>
2023-11-28 02:31:04 +0900

    add:取引遷移ボタンと取引キャンセルボタン及びテスト用の状態変更ボタンを設置

--- src/app/transactions/[transactionId]/page.tsx
+++ src/app/transactions/[transactionId]/page.tsx
@@ -1,6 +1,9 @@
 import { MessageSection } from "@/app/transactions/[transactionId]/MessageSection";
 import { TransactionStatus } from "@/app/transactions/[transactionId]/TransactionStatus";
-import { TransactionChangeButton } from "@/app/transactions/[transactionId]/TransactionChangeButton";
+import {
+  TransactionChangeButton,
+  TestTransactionChangeButton,
+} from "@/app/transactions/[transactionId]/TransactionChangeButton";
 import defaultIcon from "@/images/profile-pic-placeholder.png";
 import { findTransaction } from "@/services/transaction";
 import { getSessionUser } from "@/utils/session";
@@ -38,38 +41,55 @@ export default async function Transaction({
   }
 
   return (
-    <div className="flex w-full flex-col gap-4 py-4 lg:flex-row">
-      <aside className="flex flex-1 flex-col gap-8">
-        <TransactionStatus
-          transaction={transaction}
-          sessionUser={sessionUser}
-        />
-        <TransactionChangeButton transaction={transaction} status={4}>
-          支払完了
-        </TransactionChangeButton>
-        <div>
-          <p>出品者情報</p>
-          {/* TODO: 出品者情報へのリンクもしくはモーダルを追加 */}
-          <div className="btn btn-ghost flex h-20 items-center justify-between px-0 normal-case">
-            <div className="flex items-center gap-4">
-              <div className="avatar">
-                <div className="w-16 rounded-full">
-                  <Image
-                    src={transaction.listing.seller.image || defaultIcon}
-                    width={64}
-                    height={64}
-                    alt=""
-                  />
+    <>
+      <div className="flex w-full flex-col gap-4 py-4 lg:flex-row">
+        <aside className="flex flex-1 flex-col gap-8">
+          <TransactionStatus
+            transaction={transaction}
+            sessionUser={sessionUser}
+          />
+          <TransactionChangeButton transaction={transaction} />
+          <TransactionChangeButton transaction={transaction} isCancel={true} />
+          <div>
+            <p>出品者情報</p>
+            {/* TODO: 出品者情報へのリンクもしくはモーダルを追加 */}
+            <div className="btn btn-ghost flex h-20 items-center justify-between px-0 normal-case">
+              <div className="flex items-center gap-4">
+                <div className="avatar">
+                  <div className="w-16 rounded-full">
+                    <Image
+                      src={transaction.listing.seller.image || defaultIcon}
+                      width={64}
+                      height={64}
+                      alt=""
+                    />
+                  </div>
                 </div>
+                <span className="text-xl">
+                  {transaction.listing.seller.name}
+                </span>
               </div>
-              <span className="text-xl">{transaction.listing.seller.name}</span>
+              <FaChevronRight />
             </div>
-            <FaChevronRight />
           </div>
-        </div>
-      </aside>
-
-      <MessageSection transaction={transaction} sessionUser={sessionUser} />
-    </div>
+        </aside>
+        <MessageSection transaction={transaction} sessionUser={sessionUser} />
+      </div>
+      <TestTransactionChangeButton transaction={transaction} status={0}>
+        取引状態:支払い前
+      </TestTransactionChangeButton>
+      <TestTransactionChangeButton transaction={transaction} status={1}>
+        取引状態:支払い完了
+      </TestTransactionChangeButton>
+      <TestTransactionChangeButton transaction={transaction} status={2}>
+        取引状態:発送済み
+      </TestTransactionChangeButton>
+      <TestTransactionChangeButton transaction={transaction} status={3}>
+        取引状態:受取完了
+      </TestTransactionChangeButton>
+      <TestTransactionChangeButton transaction={transaction} status={4}>
+        取引状態:キャンセル
+      </TestTransactionChangeButton>
+    </>
   );
 }
