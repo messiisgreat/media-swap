10d43986d4a8910c0d5b6fbc4bca5cae88d96705
messiisgreat <superonecoderman@gmail.com>
2023-11-30 21:36:57 +0900

    forbid-elementsのルール追加

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -69,5 +69,9 @@ module.exports = {
         descriptionStyle: "body",
       },
     ],
+    "react/forbid-elements": [
+      "error",
+      { forbid: ["h1", "h2", "h3", "h4", "h5", "h6"] },
+    ],
   },
 };
