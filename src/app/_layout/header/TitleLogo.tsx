c5d54fadcdb2d3fe2110b76dc6f11fb0f397f73e
messiisgreat <superonecoderman@gmail.com>
2023-11-30 23:26:35 +0900

    update: iconの差し替え

--- src/app/_layout/header/TitleLogo.tsx
+++ src/app/_layout/header/TitleLogo.tsx
@@ -15,7 +15,7 @@ type TitleLogoProps = {
 export const TitleLogo = ({ className }: TitleLogoProps) => {
   return (
     <Link href="/" className={`btn btn-ghost ${className}`}>
-      <Image src={logo} height={40} width={40} alt={SITE_NAME} />
+      <Image src={logo} height={36} width={36} alt={SITE_NAME} />
       <H className="hidden text-xl normal-case md:inline">{SITE_NAME}</H>
     </Link>
   );
