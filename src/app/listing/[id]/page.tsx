22fd40e9666cc045cc328bb226da8c1773a8a733
messiisgreat <superonecoderman@gmail.com>
2023-12-02 00:12:45 +0900

    refactor:説明と情報をコンポーネントに分離

--- src/app/listing/[id]/page.tsx
+++ src/app/listing/[id]/page.tsx
@@ -1,5 +1,9 @@
 import Carousel from "@/app/listing/[id]/Carousel";
 import { CommentSection } from "@/app/listing/[id]/CommentSection";
+import {
+  ItemDescription,
+  ItemInfomation,
+} from "@/app/listing/[id]/ItemInformation";
 import { PurchaseButton } from "@/app/listing/[id]/PurchaseButton";
 import { Badge } from "@/components/Badge";
 import { ButtonAsLink } from "@/components/Button";
@@ -9,7 +13,6 @@ import { H } from "@/components/structure/H";
 import { findListingById } from "@/services/listing";
 import { getSessionUser } from "@/utils/session";
 import { Metadata } from "next";
-import Link from "next/link";
 
 type ListingPageProps = {
   params: {
@@ -47,83 +50,34 @@ export default async function ListingPage({
   params: { id },
 }: ListingPageProps) {
   const listing = await findListingById(id);
-  const tags = listing.tags.map((t) => t.tag);
   const images = listing.images;
   const user = (await getSessionUser()) || null;
   const userId = user?.id;
-  const isOwner = userId === listing.sellerId; //出品者かどうかで表示を変えられるので、後で活用する
-  const dtClass = "my-2 w-full rounded-lg bg-white p-4 shadow-md";
-  const ddClass = "font-semibold text-gray-700";
-  const ddColor = "text-gray-600";
+  const isOwner = userId === listing.sellerId;
 
   return (
     <VerifyProvider>
       <Carousel images={images} />
       {/* FIXME: 本来は、w-fullを全体にかけたいが影響範囲が大きいため一時的にラップしている  */}
-      <div className="w-full">
+      <div className="flex justify-around">
         <H className="text-left text-lg font-bold lg:text-2xl">
           {listing.productName!}
         </H>
+        <Badge className="badge-lg">¥{listing.price}</Badge>
       </div>
       <Section className="flex w-full flex-col items-start gap-4">
-        <div className="flex flex-wrap gap-2">
-          {tags.map((tag) => (
-            <Link key={tag.id} href={`/search?tagid=${tag.id}`}>
-              <Badge className="badge-lg cursor-pointer border-none bg-yellow-400 font-medium shadow-md">
-                {tag.text}
-              </Badge>
-            </Link>
-          ))}
-        </div>
-        <Badge className="badge-lg">¥{listing.price}</Badge>
         <TitleUnderbar title="説明" />
-        <div className={dtClass}>
-          <p className="my-4 text-gray-600">{listing.description}</p>
-        </div>
+        <ItemDescription listing={listing} />
         <TitleUnderbar title="商品情報" />
-        <div className={dtClass}>
-          <div className="mb-4">
-            <p className={ddClass}>商品の状態</p>
-            <p className={ddColor}>{listing.productCondition?.name}</p>
-          </div>
-          <div className="mb-4">
-            <p className={ddClass}>タグ名</p>
-            <div className="flex flex-wrap gap-2">
-              {listing.tags.map((tag) => (
-                <p
-                  key={tag.id}
-                  className="rounded-full bg-blue-100 px-2 py-1 text-blue-800"
-                >
-                  {tag.tag.text}
-                </p>
-              ))}
-            </div>
-          </div>
-          <div className="mb-4">
-            <div className="grid grid-cols-2 gap-2">
-              <p className={ddClass}>配送方法</p>
-              <p className={ddColor}>{listing.shippingMethod?.name}</p>
-              <p className={ddClass}>送料の負担について</p>
-              <p className={ddColor}>
-                {listing.shippingMethod?.amount
-                  ? "送料別(購入者負担)"
-                  : "送料込み(出品者負担)"}
-              </p>
-              <p className={ddClass}>配送までの日数</p>
-              <p className={ddColor}>{listing.shippingDays?.maxDays}日以内</p>
-            </div>
-          </div>
-        </div>
+        <ItemInfomation listing={listing} />
+
         {listing.transactionId ? (
-          <div>
-            <p>すでに商品を購入しています！</p>
-            <ButtonAsLink
-              href={`/transactions/${listing.transactionId}`}
-              secondary
-            >
-              取引へ進む
-            </ButtonAsLink>
-          </div>
+          <ButtonAsLink
+            href={`/transactions/${listing.transactionId}`}
+            secondary
+          >
+            取引へ進む
+          </ButtonAsLink>
         ) : (
           !isOwner && (
             <PurchaseButton
@@ -133,13 +87,14 @@ export default async function ListingPage({
             />
           )
         )}
+
+        <TitleUnderbar title="コメント" />
+        <CommentSection
+          listingId={listing.id}
+          sessionUser={user}
+          isListingOwner={isOwner}
+        />
       </Section>
-      <TitleUnderbar title="コメント" />
-      <CommentSection
-        listingId={listing.id}
-        sessionUser={user}
-        isListingOwner={isOwner}
-      />
     </VerifyProvider>
   );
 }
