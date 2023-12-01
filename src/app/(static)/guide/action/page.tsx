c79a8846c0ef941ff68f8d2119a4707b052f9bc0
messiisgreat <superonecoderman@gmail.com>
2023-12-02 00:00:11 +0900

    Hコンポーネント使用直後の要素をSectionコンポーネントでラップする

--- src/app/(static)/guide/action/page.tsx
+++ src/app/(static)/guide/action/page.tsx
@@ -1,3 +1,4 @@
+import { Section } from "@/components/structure";
 import { H } from "@/components/structure/H";
 import { SITE_NAME } from "@/constants/site";
 
@@ -9,81 +10,83 @@ export default function GuideActionPage() {
     <div>
       <div className="border-2">
         <H className="text-center text-xl font-bold">{SITE_NAME}ガイド</H>
-        <div className="py-6">
-          <H className="text-lg font-bold">禁止されている行為</H>
-          <p className="py-4">
-            {SITE_NAME}
-            は、誰しもが商品を手軽に購入や販売できるフリマプラットフォームです。
-            多数のユーザーが心置きなく安全に取引を行えるため、以下の活動を制限しております。
-          </p>
-          <p className="py-4">
-            これらの活動を検知した際には、アカウントの使用制約などの処置を考慮することがございます。
-          </p>
-        </div>
-        <div className="py-6">
-          <H className="text-lg font-bold">取引に関する注意点</H>
-          <ul className="list-disc px-4">
-            <li>{SITE_NAME}が提供していない支払い手段の使用を推奨すること</li>
-            <li>商品情報が不明確な取引</li>
-            <li>{SITE_NAME}の指定した取引手順に従わない活動</li>
-            <li>資金洗浄と見なされる活動</li>
-            <li>
-              商品を出品した者やその家族、関連する人物がその商品を購入すること
-            </li>
-            <li>商品の交換や部分的な交換</li>
-          </ul>
-        </div>
-        <div className="py-6">
-          <H className="text-lg font-bold">発送に関する注意</H>
-          <ul className="list-disc px-4">
-            <li>
-              価格に送料が含まれている商品を、送料を除外して(着払い)で送る行為
-            </li>
-            <li>商品の到着先を郵便局や営業所での保管にする行為</li>
-            <li>商品の直接の受け渡しを求める行為</li>
-            <li>支払い前に出品者に商品の発送を要求する行為</li>
-            <li>受取が困難な場所を商品の到着先として選択する行為</li>
-          </ul>
-        </div>
-        <div className="py-6">
-          <H className="text-lg font-bold">出品に関する注意点</H>
-          <ul className="list-disc px-4">
-            <li>
-              法的な許可や報告、ライセンスが必要なアイテムを、それらの確認なしにリスト化すること
-            </li>
-            <li>商品の具体的な状態を示す写真を載せないこと</li>
-            <li>
-              保有していないアイテムのリスト化や、他のECサイトからの直接配送を行うこと
-            </li>
-            <li>実際の販売意図のない出品</li>
-            <li>他の人の商品を代わりにリスト化すること</li>
-            <li>オークション方式での出品</li>
-            <li>
-              購入ボタンをクリックしても商品が最終的に確定しない出品スタイル
-            </li>
-            <li>
-              商品に不具合があったとしても返品を受け付けない旨を記述すること
-            </li>
-            <li>虚偽の設定、または誤った情報を記載すること</li>
-            <li>他会員が撮影した画像、文章などを無断で使用すること</li>
-          </ul>
-        </div>
-        <div className="py-6">
-          <H className="text-lg font-bold">その他</H>
-          <ul className="list-disc px-4">
-            <li>他のサービスへの誘導</li>
-            <li>不快と感じる行為</li>
-            <li>他者を勧誘する活動</li>
-            <li>真実でない情報の掲載</li>
-            <li>個人情報を伴うリスト・投稿、またはその不正な使用</li>
-            <li>{SITE_NAME}アカウントの不正な活用</li>
-            <li>非合法的な行為やそれを助長する行為</li>
-            <li>人々の間での差別感を生じさせる行為</li>
-            <li>一般的な道徳に違反する行為</li>
-            <li>選挙活動に関連する行為</li>
-            <li>その他、適さないと見なされる行為</li>
-          </ul>
-        </div>
+        <Section>
+          <div className="py-6">
+            <H className="text-lg font-bold">禁止されている行為</H>
+            <p className="py-4">
+              {SITE_NAME}
+              は、誰しもが商品を手軽に購入や販売できるフリマプラットフォームです。
+              多数のユーザーが心置きなく安全に取引を行えるため、以下の活動を制限しております。
+            </p>
+            <p className="py-4">
+              これらの活動を検知した際には、アカウントの使用制約などの処置を考慮することがございます。
+            </p>
+          </div>
+          <div className="py-6">
+            <H className="text-lg font-bold">取引に関する注意点</H>
+            <ul className="list-disc px-4">
+              <li>{SITE_NAME}が提供していない支払い手段の使用を推奨すること</li>
+              <li>商品情報が不明確な取引</li>
+              <li>{SITE_NAME}の指定した取引手順に従わない活動</li>
+              <li>資金洗浄と見なされる活動</li>
+              <li>
+                商品を出品した者やその家族、関連する人物がその商品を購入すること
+              </li>
+              <li>商品の交換や部分的な交換</li>
+            </ul>
+          </div>
+          <div className="py-6">
+            <H className="text-lg font-bold">発送に関する注意</H>
+            <ul className="list-disc px-4">
+              <li>
+                価格に送料が含まれている商品を、送料を除外して(着払い)で送る行為
+              </li>
+              <li>商品の到着先を郵便局や営業所での保管にする行為</li>
+              <li>商品の直接の受け渡しを求める行為</li>
+              <li>支払い前に出品者に商品の発送を要求する行為</li>
+              <li>受取が困難な場所を商品の到着先として選択する行為</li>
+            </ul>
+          </div>
+          <div className="py-6">
+            <H className="text-lg font-bold">出品に関する注意点</H>
+            <ul className="list-disc px-4">
+              <li>
+                法的な許可や報告、ライセンスが必要なアイテムを、それらの確認なしにリスト化すること
+              </li>
+              <li>商品の具体的な状態を示す写真を載せないこと</li>
+              <li>
+                保有していないアイテムのリスト化や、他のECサイトからの直接配送を行うこと
+              </li>
+              <li>実際の販売意図のない出品</li>
+              <li>他の人の商品を代わりにリスト化すること</li>
+              <li>オークション方式での出品</li>
+              <li>
+                購入ボタンをクリックしても商品が最終的に確定しない出品スタイル
+              </li>
+              <li>
+                商品に不具合があったとしても返品を受け付けない旨を記述すること
+              </li>
+              <li>虚偽の設定、または誤った情報を記載すること</li>
+              <li>他会員が撮影した画像、文章などを無断で使用すること</li>
+            </ul>
+          </div>
+          <div className="py-6">
+            <H className="text-lg font-bold">その他</H>
+            <ul className="list-disc px-4">
+              <li>他のサービスへの誘導</li>
+              <li>不快と感じる行為</li>
+              <li>他者を勧誘する活動</li>
+              <li>真実でない情報の掲載</li>
+              <li>個人情報を伴うリスト・投稿、またはその不正な使用</li>
+              <li>{SITE_NAME}アカウントの不正な活用</li>
+              <li>非合法的な行為やそれを助長する行為</li>
+              <li>人々の間での差別感を生じさせる行為</li>
+              <li>一般的な道徳に違反する行為</li>
+              <li>選挙活動に関連する行為</li>
+              <li>その他、適さないと見なされる行為</li>
+            </ul>
+          </div>
+        </Section>
       </div>
     </div>
   );
--- src/app/(static)/guide/product/page.tsx
+++ src/app/(static)/guide/product/page.tsx
@@ -1,3 +1,4 @@
+import { Section } from "@/components/structure";
 import { H } from "@/components/structure/H";
 import { SITE_NAME } from "@/constants/site";
 
@@ -9,29 +10,33 @@ export default function GuideListingPage() {
     <div>
       <div className="border-2">
         <H className="text-center text-xl font-bold">{SITE_NAME}ガイド</H>
-        <div className="py-6">
-          <H className="text-lg font-bold">禁止されている出品物</H>
-          <p className="py-4">
-            {SITE_NAME}
-            は、誰しもが商品を手軽に購入や販売できるフリマプラットフォームです。
-            多数のユーザーが心置きなく安全に取引を行えるため、以下の活動を制限しております。
-          </p>
-          <p className="py-4">
-            これらの活動を検知した際には、アカウントの使用制約などの処置を考慮することがございます。
-          </p>
-        </div>
-        <div className="py-6">
-          <ul className="list-disc px-4">
-            <li>ダウンロードコンテンツやデジタルコンテンツなどの電子データ</li>
-            <li>不正な手段で取得したアイテムや盗品</li>
-            <li>犯罪や違法行為に使用される可能性があるもの</li>
-            <li>危険物や安全性に問題があるもの</li>
-            <li>児童ポルノやそれに類するとみなされるもの</li>
-            <li>手元にない商品の出品やECサイト等から直送すること</li>
-            <li>個人情報を含む出品・投稿、個人情報の不正利用</li>
-            <li>その他、不適切と判断されるもの</li>
-          </ul>
-        </div>
+        <Section>
+          <div className="py-6">
+            <H className="text-lg font-bold">禁止されている出品物</H>
+            <p className="py-4">
+              {SITE_NAME}
+              は、誰しもが商品を手軽に購入や販売できるフリマプラットフォームです。
+              多数のユーザーが心置きなく安全に取引を行えるため、以下の活動を制限しております。
+            </p>
+            <p className="py-4">
+              これらの活動を検知した際には、アカウントの使用制約などの処置を考慮することがございます。
+            </p>
+          </div>
+          <div className="py-6">
+            <ul className="list-disc px-4">
+              <li>
+                ダウンロードコンテンツやデジタルコンテンツなどの電子データ
+              </li>
+              <li>不正な手段で取得したアイテムや盗品</li>
+              <li>犯罪や違法行為に使用される可能性があるもの</li>
+              <li>危険物や安全性に問題があるもの</li>
+              <li>児童ポルノやそれに類するとみなされるもの</li>
+              <li>手元にない商品の出品やECサイト等から直送すること</li>
+              <li>個人情報を含む出品・投稿、個人情報の不正利用</li>
+              <li>その他、不適切と判断されるもの</li>
+            </ul>
+          </div>
+        </Section>
       </div>
     </div>
   );
