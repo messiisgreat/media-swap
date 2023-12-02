2d7fb4fe0251dc26260fc6dae4237ffd34f49797
messiisgreat <superonecoderman@gmail.com>
2023-12-02 17:55:54 +0900

    lint修正

--- src/app/listing/[id]/CommentSection.tsx
+++ src/app/listing/[id]/CommentSection.tsx
@@ -141,7 +141,7 @@ export const CommentSection = ({
                       {parseRelativeTime(comment.createdAt)}
                     </p>
                     {sessionUser ? (
-                      <div className="dropdown-end dropdown-bottom dropdown">
+                      <div className="dropdown dropdown-end dropdown-bottom">
                         <label
                           tabIndex={0}
                           className="btn btn-ghost h-[initial] min-h-0 p-2"
@@ -150,7 +150,7 @@ export const CommentSection = ({
                         </label>
                         <ul
                           tabIndex={0}
-                          className="menu dropdown-content rounded-box z-[1] w-24 gap-2 bg-base-100 p-2 text-red-500 shadow"
+                          className="menu dropdown-content z-[1] w-24 gap-2 rounded-box bg-base-100 p-2 text-red-500 shadow"
                         >
                           {comment.userId !== sessionUser.id ? (
                             <li
