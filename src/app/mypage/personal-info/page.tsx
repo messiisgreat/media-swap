db80e3e2f8d8f385a5a9e81d80bfd1dade241cc1
messiisgreat <superonecoderman@gmail.com>
2023-12-03 19:48:48 +0900

    fix:個人情報ページのタイトルを定数に変更

--- src/app/mypage/personal-info/page.tsx
+++ src/app/mypage/personal-info/page.tsx
@@ -1,6 +1,7 @@
 import { AddressForm } from "@/app/mypage/personal-info/AddressForm";
 import { VerifyProvider } from "@/components/form/securityVerifier/VerifyProvider";
 import { PageTitle, Section } from "@/components/structure";
+import { PAGE_CONTENT, PAGE_CONTENT_ENUM_JA } from "@/constants/myPage";
 
 /**
  *住所変更ページ
@@ -8,7 +9,7 @@ import { PageTitle, Section } from "@/components/structure";
 export default async function Page() {
   return (
     <>
-      <PageTitle title="登録情報変更" />
+      <PageTitle title={PAGE_CONTENT_ENUM_JA[PAGE_CONTENT.PRIVACY]} />
       <Section>
         <VerifyProvider>
           <AddressForm />
