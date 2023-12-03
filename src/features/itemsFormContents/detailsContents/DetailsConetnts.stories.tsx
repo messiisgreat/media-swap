ca37ee5e25f338dde77dcadb916b291107a9dc09
messiisgreat <superonecoderman@gmail.com>
2023-12-04 04:07:36 +0900

    Add DetailsContents and ListingTagsInput components

---
+++ src/features/itemsFormContents/detailsContents/DetailsConetnts.stories.tsx
@@ -0,0 +1,51 @@
+import { DetailsContents } from "@/features/itemsFormContents/detailsContents/DetailsContents";
+import { Meta, StoryObj } from "@storybook/react";
+
+export default {
+  component: DetailsContents,
+  parameters: {
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  decorators: [
+    (Story) => (
+      <div className="my-16 flex flex-col items-center">
+        <Story />
+      </div>
+    ),
+  ],
+} satisfies Meta<typeof DetailsContents>;
+
+type Story = StoryObj<typeof DetailsContents>;
+
+export const Empty: Story = {
+  args: {
+    productName: "",
+    productConditionId: "",
+    description: "",
+    tags: [],
+  },
+};
+
+export const Filled: Story = {
+  args: {
+    productName: "商品名",
+    productConditionId: "1",
+    description: "商品説明",
+    tags: [
+      {
+        id: "1",
+        text: "タグ名",
+        createdAt: new Date(),
+      },
+    ],
+    selectedTags: [
+      {
+        id: "1",
+        text: "タグ名",
+        createdAt: new Date(),
+      },
+    ],
+  },
+};
