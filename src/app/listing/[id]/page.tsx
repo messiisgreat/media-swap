d04f0682d4caf21b972431349f9066da5040a928
messiisgreat <superonecoderman@gmail.com>
2023-12-05 03:22:45 +0900

    スクロール時にギャラリーを中央に寄せる

--- src/app/listing/[id]/page.tsx
+++ src/app/listing/[id]/page.tsx
@@ -1,4 +1,5 @@
-import Carousel from "@/app/listing/[id]/Carousel";
+// import Carousel from "@/app/listing/[id]/Carousel";
+import TempCarousel from "@/app/listing/[id]/TempCarousel";
 import { CommentSection } from "@/app/listing/[id]/CommentSection";
 import { ItemDescription } from "@/app/listing/[id]/ItemDescription";
 import { ItemInfomation } from "@/app/listing/[id]/ItemInformation";
@@ -55,7 +56,7 @@ export default async function ListingPage({
 
   return (
     <VerifyProvider>
-      <Carousel images={images} />
+      <TempCarousel images={images} />
       {/* FIXME: 本来は、w-fullを全体にかけたいが影響範囲が大きいため一時的にラップしている  */}
       <div className="w-full">
         <H className="text-left text-lg font-bold lg:text-2xl">
