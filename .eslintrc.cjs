5c29bfba712224956cc7227c5493742e91d1072f
messiisgreat <superonecoderman@gmail.com>
2023-12-09 06:18:08 +0900

    fix:一時的にwarnにしていたlintを復活

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -138,13 +138,6 @@ module.exports = {
       },
     ],
 
-    "@typescript-eslint/no-unsafe-return": "warn",
-    "@typescript-eslint/no-unsafe-assignment": "warn",
-    "@typescript-eslint/no-unsafe-call": "warn",
-    "@typescript-eslint/no-unsafe-member-access": "warn",
-    "@typescript-eslint/no-unsafe-argument": "warn",
-    "@typescript-eslint/no-redundant-type-constituents": "warn",
-
     // "import/order": [
     //   "error",
     //   {
