d8eceb8fcada4b120e0b155fbfa486aaa540c7e1
messiisgreat <superonecoderman@gmail.com>
2023-12-13 03:32:14 +0900

    #486 update:transactionOptionItemsのtsx化

---
+++ src/app/(contents)/(auth)/transactions/[transactionId]/TransactionOptionItems.tsx
@@ -0,0 +1,5 @@
+import { CancelFormButton } from "@/app/(contents)/(auth)/transactions/[transactionId]/CancelFormButton";
+
+export const transactionOptionItems = [
+  <CancelFormButton key="cancelFormButton" />,
+];
