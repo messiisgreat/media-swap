baf31089303ad7ecbfe7840d32a6648a8aa9d3a4
messiisgreat <superonecoderman@gmail.com>
2023-12-15 01:55:34 +0900

    #486 update:ボタンにフォーム機能を追加

--- src/app/(contents)/(auth)/transactions/[transactionId]/CancelFormButton.tsx
+++ src/app/(contents)/(auth)/transactions/[transactionId]/CancelFormButton.tsx
@@ -1,20 +1,24 @@
 "use client";
 
+import { useCancelModal } from "@/app/(contents)/(auth)/transactions/[transactionId]/hooks";
+
 type Props = {
   /** className */
   className?: string;
 };
 
 /**
- * 
+ *
  * 取引画面のキャンセルフォームボタン
  */
-export const CancelFormButton = ({
-  className = "",
-}: Props) => {
+export const CancelFormButton = ({ className = "" }: Props) => {
+  const [handleOpen, CancelModal] = useCancelModal();
   return (
-      <button className={className}>
+    <>
+      <button onClick={handleOpen} className={className}>
         運営への問合わせ
       </button>
+      <CancelModal />
+    </>
   );
 };
