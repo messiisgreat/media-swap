bd5d77086a9e7e062bb33c3da9ef1520c3b76a03
messiisgreat <superonecoderman@gmail.com>
2023-12-16 16:21:37 +0900

    jest / testing-libraryのeslint-pluginの導入 (#607)
    * feat::テスト関連のeslint-plugin導入
    
    * fix:eslintのextendsのprettier設定修正
    
    * feat:jestのeslint推奨設定の適用とエラー箇所の修正
    
    * feat:jest-domのeslint推奨設定の適用
    
    * feat:testing-libraryのeslint推奨設定の適用とエラー箇所の修正
    
    * テストメソッドの統一
    
    * test:不要な検証削除する

--- src/features/itemsFormContents/detailsContents/ItemTagsInput.test.tsx
+++ src/features/itemsFormContents/detailsContents/ItemTagsInput.test.tsx
@@ -1,66 +1,67 @@
-import { fireEvent, render, screen, waitFor } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { userEvent } from "@testing-library/user-event";
 
 import { ItemTagsInput } from "@/features/itemsFormContents/detailsContents/ItemTagsInput";
 
 describe("ItemTagsInput", () => {
   describe("タグの追加", () => {
-    it("Enterでtagを追加できること", async () => {
+    test("Enterでタグを追加できること", async () => {
       render(<ItemTagsInput name="tag" suggestedTags={[]} />);
-
       const input = screen.getByPlaceholderText("タグ名を入力してください");
+      await userEvent.type(input, "newTag");
 
-      void userEvent.type(input, "newTag");
-      fireEvent.keyDown(input, { keyCode: 13, target: { value: "newTag" } });
+      fireEvent.keyDown(input, { keyCode: 13, target: input });
 
-      await waitFor(() => {
-        expect(input).toHaveValue("");
-        expect(screen.getByText("newTag")).toBeInTheDocument();
-        expect(screen.getByText("Tags")).toBeInTheDocument();
-      });
+      expect(screen.getByText("Tags")).toBeInTheDocument();
+      expect(screen.getByText("newTag")).toBeInTheDocument();
     });
-    it("重複して同じ名前のタグを追加できないこと", async () => {
+
+    test("タグ追加後、タグの入力欄は空になる", async () => {
       render(<ItemTagsInput name="tag" suggestedTags={[]} />);
-
       const input = screen.getByPlaceholderText("タグ名を入力してください");
-      await waitFor(() => {
-        void userEvent.type(input, "newTag");
-        fireEvent.keyDown(input, { keyCode: 13, target: { value: "newTag" } });
-        fireEvent.keyDown(input, { keyCode: 13, target: { value: "newTag" } });
+      await userEvent.type(input, "newTag");
 
-        expect(screen.getAllByText("newTag").length).toBe(1);
-      });
+      fireEvent.keyDown(input, { keyCode: 13, target: input });
+
+      expect(input).toHaveValue("");
+    });
+
+    test("重複して同じ名前のタグを追加できないこと", async () => {
+      render(<ItemTagsInput name="tag" suggestedTags={[]} />);
+      const input = screen.getByPlaceholderText("タグ名を入力してください");
+      await userEvent.type(input, "newTag");
+      fireEvent.keyDown(input, { keyCode: 13, target: input });
+
+      fireEvent.keyDown(input, { keyCode: 13, target: input });
+
+      expect(screen.getAllByText("newTag")).toHaveLength(1);
     });
   });
 
-  it("focus時にsuggestedTagsが表示されること", () => {
+  test("focus時にsuggestedTagsが表示されること", async () => {
     const suggestedTags = [
       { id: "1", text: "tag1", createdAt: new Date() },
       { id: "2", text: "tag2", createdAt: new Date() },
     ];
     render(<ItemTagsInput name="tag" suggestedTags={suggestedTags} />);
-
     const input = screen.getByPlaceholderText("タグ名を入力してください");
 
-    void userEvent.click(input);
+    await userEvent.click(input);
 
     expect(screen.getByTestId("test-tag1")).toBeInTheDocument();
     expect(screen.getByTestId("test-tag2")).toBeInTheDocument();
   });
-  it("タグの削除ができること", async () => {
+
+  test("タグの削除ができること", async () => {
     render(<ItemTagsInput name="tag" suggestedTags={[]} />);
-
     const input = screen.getByPlaceholderText("タグ名を入力してください");
-
-    void userEvent.type(input, "newTag");
-    fireEvent.keyDown(input, { keyCode: 13, target: { value: "newTag" } });
-
+    await userEvent.type(input, "newTag");
+    fireEvent.keyDown(input, { keyCode: 13, target: input });
     const deleteButton = screen.getByTestId("delete");
-    void userEvent.click(deleteButton);
 
-    await waitFor(() => {
-      expect(screen.queryByText("newTag")).not.toBeInTheDocument();
-      expect(screen.queryByText("Tags")).not.toBeInTheDocument();
-    });
+    await userEvent.click(deleteButton);
+
+    expect(screen.queryByText("newTag")).not.toBeInTheDocument();
+    expect(screen.queryByText("Tags")).not.toBeInTheDocument();
   });
 });
--- src/lib/result/result.test.ts
+++ src/lib/result/result.test.ts
@@ -1,37 +1,22 @@
 import { failure, success, type Result } from "@/lib/result";
 
 describe("Result型", () => {
-  type TestType = {
-    foo: string;
-  };
-
   describe("success", () => {
     test("成功時の値が正しく格納されること", () => {
       const value = 123;
       const result: Result<number> = success(value);
 
-      expect(result).toMatchObject({
+      expect(result).toStrictEqual({
         isSuccess: true,
         isFailure: false,
         value: value,
       });
     });
 
-    test("isSuccessで絞り込んだあとの型が正しいこと", () => {
-      const value: TestType = {
-        foo: "bar",
-      };
-      const result: Result<TestType> = success(value);
-
-      if (result.isSuccess) {
-        expect(result.value).toBeInstanceOf(Object);
-      }
-    });
-
     test("値がない場合はundefinedが格納されること", () => {
       const result: Result<number> = success();
 
-      expect(result).toMatchObject({
+      expect(result).toStrictEqual({
         isSuccess: true,
         isFailure: false,
         value: undefined,
@@ -44,22 +29,21 @@ describe("Result型", () => {
       const error = "Something went wrong";
       const result: Result<string, string> = failure(error);
 
-      expect(result).toMatchObject({
+      expect(result).toStrictEqual({
         isSuccess: false,
         isFailure: true,
         error: error,
       });
     });
 
-    test("isFailureで絞り込んだあとの型が正しいこと", () => {
-      const error: TestType = {
-        foo: "bar",
-      };
-      const result: Result<string, TestType> = failure(error);
+    test("値がない場合はundefinedが格納されること", () => {
+      const result: Result<number> = success();
 
-      if (result.isFailure) {
-        expect(result.error).toBeInstanceOf(Object);
-      }
+      expect(result).toStrictEqual({
+        isSuccess: true,
+        isFailure: false,
+        value: undefined,
+      });
     });
   });
 });
