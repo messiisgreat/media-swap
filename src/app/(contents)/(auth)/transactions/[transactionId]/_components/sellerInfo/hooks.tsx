6a7280da592ce8ab21fa3cf82bb33cea71ef955f
messiisgreat <superonecoderman@gmail.com>
2023-12-18 01:53:44 +0900

    update:#486フォーム画面設定

--- src/app/(contents)/(auth)/transactions/[transactionId]/_components/sellerInfo/hooks.tsx
+++ src/app/(contents)/(auth)/transactions/[transactionId]/_components/sellerInfo/hooks.tsx
@@ -4,28 +4,47 @@ import { useCallback } from "react";
 
 import { useFormActionModal } from "@/features/modal";
 import { H } from "@/ui/structure/H";
-
+import { Select, Textarea } from "@/ui/form";
+import {
+  cancellationBuyerReasons,
+  // cancellationSellerReasons,
+} from "@/app/(contents)/(auth)/transactions/[transactionId]/_components/sellerInfo/types";
 /**
  * 取引キャンセル用のモーダル
- * @returns 
+ * @returns
  */
 export const useCancelModal = () => {
-
   const action = async () => {
-    // await new Promise<void>((resolve) => setTimeout(resolve, 1000));
-  }
+    await new Promise<void>((resolve) => setTimeout(resolve, 1000));
+  };
   const { handleOpen, FormActionModal } = useFormActionModal(action, "送信");
 
   const CancelModal = useCallback(
     () => (
       <FormActionModal>
         <H className="text-center text-lg font-bold">取引キャンセルの問合せ</H>
-        <p className="py-2">以下の項目に取引キャンセルの内容を入力してください</p>
-        <dl className="mb-4 grid grid-cols-2 gap-2 " role="alert">
-        <div>テスト</div>
-        </dl>
+        <p className="whitespace-normal">
+          以下の項目に取引キャンセルの内容を入力してください
+        </p>
+        <Select
+          labelText="お問い合わせ種別"
+          id="category"
+          name="category"
+          options={cancellationBuyerReasons}
+          required
+        />
+        <Textarea
+          labelText="お問い合わせ内容"
+          id="body"
+          placeholder="お問い合わせ内容を入力してください。"
+          name="body"
+          required
+          maxLength={4000}
+        />
       </FormActionModal>
-    ), [FormActionModal]);
+    ),
+    [FormActionModal],
+  );
 
   return [handleOpen, CancelModal] as const;
 };
