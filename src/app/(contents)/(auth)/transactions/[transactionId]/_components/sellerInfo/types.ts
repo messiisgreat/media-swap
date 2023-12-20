7a79ed6e19425f8ba57f8932ab3ca050eeed9762
messiisgreat <superonecoderman@gmail.com>
2023-12-21 01:46:30 +0900

    #486 add:定数追加

--- src/app/(contents)/(auth)/transactions/[transactionId]/_components/sellerInfo/types.ts
+++ src/app/(contents)/(auth)/transactions/[transactionId]/_components/sellerInfo/types.ts
@@ -1,3 +1,34 @@
+import { type ZodType, z } from "zod";
+
+import { type FormState } from "@/ui/form";
+
+type InCancellationFormValues = {
+  name: string;
+  email: string;
+  category: string;
+  body: string;
+};
+
+export type CancellationInquiryFormState = FormState<InCancellationFormValues>;
+
+export const initialCancellationFormValues = {
+  values: {
+    name: "maeda yuki",
+    email: "y.maeda@valour-tec.com",
+    category: "",
+    body: "",
+    verificationCode: "",
+  },
+  errors: {},
+  message: "",
+};
+
+export const cancellationInquiryFormSchema: ZodType<InCancellationFormValues> = z.object({
+  name: z.string().min(1, { message: "お名前を入力してください" }),
+  email: z.string().email({ message: "メールアドレスが不正です" }),
+  category: z.string().min(1, { message: "カテゴリーを選択してください" }),
+  body: z.string().min(1, { message: "本文は必須です" }),
+});
 
 /** キャンセル理由(出品者) */
 export const cancellationSellerReasons = {
