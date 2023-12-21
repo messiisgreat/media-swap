6be7ed23b29a07a485b1303f20c97ac3437bea6b
messiisgreat <superonecoderman@gmail.com>
2023-12-21 10:50:19 +0900

    feat: ログインボタン/メニューを改善 (#633)
    * feat: ヘッダーを変更
    
    * fix: 見た目修正
    
    * fix: ログインボタン

--- src/app/_layout/header/UserMenuButton.tsx
+++ src/app/_layout/header/UserMenuButton.tsx
@@ -6,9 +6,14 @@ import { type Session } from "next-auth";
 import { signIn, signOut } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
+import { FaSignInAlt, FaTags, FaSignOutAlt } from "react-icons/fa";
+import { FaBagShopping, FaHeartCircleCheck } from "react-icons/fa6";
+import { TbGitPullRequestDraft } from "react-icons/tb";
 
 import profilePicPlaceholder from "@/images/profile-pic-placeholder.png";
 import { ThreeDotsIcon } from "@/app/_layout/header/ThreeDotsIcon";
+import { Button } from "@/ui";
+import { PAGE_CONTENT_ENUM_JA } from "@/constants/myPage";
 
 type UserMenuButtonProps = {
   session: Session | null;
@@ -53,34 +58,49 @@ export default function UserMenuButton({ session }: UserMenuButtonProps) {
   }, []);
 
   return (
-    <details ref={dropdownRef} className="dropdown">
-      <summary className="btn btn-circle btn-ghost">
-        {user ? (
-          <Image
-            src={user?.image || profilePicPlaceholder}
-            alt="Profile picture"
-            width={40}
-            height={40}
-            className="w-10 rounded-full"
-          />
-        ) : (
-          <ThreeDotsIcon />
-        )}
-      </summary>
-      <ul className="menu dropdown-content menu-sm absolute right-0 z-30 mt-3 w-52 rounded-box bg-base-100 p-2 shadow">
-        {user ? (
-          <>
+    <div className="w-full">
+      {user ? (
+        <details ref={dropdownRef} className="dropdown">
+          <summary className="btn btn-circle btn-ghost">
+            {user ? (
+              <Image
+                src={user?.image || profilePicPlaceholder}
+                alt="Profile picture"
+                width={40}
+                height={40}
+                className="w-10 rounded-full"
+              />
+            ) : (
+              <ThreeDotsIcon />
+            )}
+          </summary>
+          <ul className="menu dropdown-content menu-lg absolute right-0 z-30 mt-3 w-64 rounded-box bg-base-100 p-2 shadow md:menu-md md:w-52">
             <li>
-              <button onClick={handleSignOut}>Sign Out</button>
+              <Link href="/mypage"><div className="flex flex-col">
+                <p className="text-base font-bold md:text-sm">{user.name}</p><p className="text-sm text-zinc-400 md:text-xs">{user.email}</p></div></Link>
             </li>
             <li>
-              <Link href="/mypage">マイページ</Link>
+              <Link href="/mypage/listings"><FaBagShopping /> {PAGE_CONTENT_ENUM_JA["items"]}</Link>
             </li>
-          </>
-        ) : (
-          <button onClick={handleSignIn}>Sign In</button>
-        )}
-      </ul>
-    </details>
+            <li>
+              <Link href="/mypage/purchases"><FaTags />{PAGE_CONTENT_ENUM_JA["purchases"]}</Link>
+            </li>
+            <li>
+              <Link href="/mypage/draft"><TbGitPullRequestDraft /> {PAGE_CONTENT_ENUM_JA["draft"]}</Link>
+            </li>
+            <li>
+              <Link href="/mypage/likes"><FaHeartCircleCheck /> {PAGE_CONTENT_ENUM_JA["likes"]}</Link>
+            </li>
+            <li>
+              <button onClick={handleSignOut}><FaSignOutAlt /> サインアウト</button>
+            </li>
+          </ul>
+        </details>
+      ) : (
+        <Button onClick={handleSignIn}>
+          <FaSignInAlt size="1.5rem" /><span className="hidden md:inline">{" "}ログイン</span>
+        </Button>
+      )}
+    </div>
   );
 }
